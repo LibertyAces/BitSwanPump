3f2b2a30ddfc650fd066319e0e2927bc26191332
Jan Pišl <jan.pisl@teskalabs.com>
2018-03-07 19:45:19 +0100

    Elasticsearch sink updated.

--- bspump/elasticsearch/essink.py
+++ bspump/elasticsearch/essink.py
@@ -15,15 +15,18 @@ L = logging.getLogger(__name__)
 class ElasticSearchDriver(object):
 
 	def __init__(self, app):
-		self.output_bucket_max_size = 1000
-		self.output_bucket = ""
 		self.output_queue = asyncio.Queue(maxsize=1000, loop=app.Loop)
 
-		self.url = asab.Config["elasticsearch"]["url"].strip()
-		if self.url[-1] != '/':
-			self.url += '/'
-		self.url += 'write?db=' + asab.Config["elasticsearch"]["db"]
+		self.uri = Config['elasticsearch']['uri'].strip()
+		if self.uri[-1] != '/': self.uri += '/'
+		self.uri_bulk = self.uri + '_bulk'
 
+		self.bulk_out_max_size = int(Config['elasticsearch']['bulk_out_max_size'])
+		self.bulk_out = ""
+
+		self.timeout = float(Config['elasticsearch']['timeout'])
+		self.rollover_type = Config['elasticsearch']['rollover_mechanism']
+		self.max_index_size = int(Config['elasticsearch']['max_index_size'])
 
 		app.PubSub.subscribe("Application.tick!", self.flush)
 		app.PubSub.subscribe("Application.exit!", self.flush)
@@ -31,25 +34,24 @@ class ElasticSearchDriver(object):
 		future = asyncio.Future()
 		asyncio.ensure_future(self._submit(future))
 
-
 	def consume(self, data):
-		self.output_bucket += data
-		if len(self.output_bucket) > self.output_bucket_max_size:
+		self.bulk_out += data
+
+		if len(self.bulk_out) > self.bulk_out_max_size:
 			self.flush()
 
 	def flush(self, event_name=None):
-		if len(self.output_bucket) == 0:
+		if len(self.bulk_out) == 0:
 			return
 
-		self.output_queue.put_nowait(self.output_bucket)
-		self.output_bucket = ""
-
+		self.output_queue.put_nowait(self.bulk_out)
+		self.bulk_out = ""
 
 	async def _submit(self, future):
 		while True:
-			output_bucket = await self.output_queue.get()
+			bulk_out = await self.output_queue.get()
 			async with aiohttp.ClientSession() as session:
-				async with session.post('', data=output_bucket) as resp:
+				async with session.post('', data=bulk_out) as resp:
 					if resp.status_code != 200:
 						L.error("Failed to insert document into ElasticSearch status:{} body:{}".format(resp.status_code, resp.text))
 						raise RuntimeError("Failed to insert document into ElasticSearch")
@@ -63,6 +65,57 @@ class ElasticSearchDriver(object):
 				break
 		future.set_result("done")
 
+	def update_index(self, es_type, version, es_index_period, orig_index=None):		
+		if self.rollover_type == 'index_size_based':
+			return self.index_size_based_update(es_type, version, es_index_period, orig_index)
+		elif self.rollover_type == 'time_based':
+			return self.time_based_update(es_type, version, es_index_period)
+		else: 
+			L.error("Invalid rollover mechanism: '{}'. Allowed values are 'index_size_based' or 'time_based'.").format(self.rollover_type)
+			raise RuntimeError("Index rollover failed.")
+
+	def _generate_es_index(self, es_type, version, seqno):
+		return "{}_{}{:05}".format(es_type, version, seqno)
+
+	def index_size_based_update(self, es_type, version, es_index_period, orig_index):
+		
+		if orig_index is not None:
+			# Get actual size of the currently active index
+			uri_get_index_size = self.uri + '{}/_stats/store'.format(orig_index)
+			stats = self.session.get(uri_get_index_size, timeout=self.timeout)
+			data = stats.json()
+
+			if data.get('_all') is None:
+				current_index_size = 0
+			else:
+				current_index_size = data['_all']['primaries']['store']['size_in_bytes']
+		else:
+			current_index_size = 0
+
+		if (orig_index is None) or (current_index_size >= self.max_index_size):
+			# Roll over to a new index
+			seqno = int((time.time() - 1500000000) / es_index_period)
+			new_es_index = self._generate_es_index(es_type, version, seqno)
+
+			if new_es_index == orig_index:
+				L.warning("ElasticSearch index name update resulted in original name, decrease es_index_period value.")
+
+			return new_es_index 		
+
+		else:
+			return orig_index
+
+	def time_based_update(self, es_type, version, es_index_period):
+		seqno = int((time.time() - 1500000000) / es_index_period)
+		return self._generate_es_index(es_type, version, seqno)
+
+	def parse_index_period(self, value):
+		if value == 'd': return 60*60*24 # 1 day
+		elif value == 'w': return 60*60*24*7 # 7 days
+		elif value == 'm': return 60*60*24*28 # 28 days
+
+		return int(value) # Otherwise use value in seconds
+
 
 class ElasticSearchSink(bspump.Sink):
 
@@ -71,7 +124,6 @@ class ElasticSearchSink(bspump.Sink):
 		self._driver = driver
 
 	def process(self, event):
-		# Processing data by line
-		wire_line = "{},{} {} {}\n".format(
-			event['measurement'], event['tag_set'], event['field_set'], int(event['timestamp']))
-		self._driver.consume(wire_line)
+		data = '{{"index": {{ "_index": "{}", "_type": "{}" }}\n{}\n'.format(es_index, es_type, json.dumps(obj))
+		self._driver.consume(data)
+
