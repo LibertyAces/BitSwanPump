3853338892e8545877f6dd406395a15581e9a8ef
Ales Teska <ales.teska@teskalabs.com>
2018-03-10 14:28:41 +0100

    Simplyfing the example.

--- bspump-amqp.py
+++ bspump-amqp.py
@@ -4,32 +4,23 @@ import bspump.socket
 import bspump.common
 import bspump.amqp
 
-
 class SamplePipeline1(bspump.Pipeline):
 
 	def __init__(self, app, pipeline_id, driver):
 		super().__init__(app, pipeline_id)
-
-		self.driver = driver
-
 		self.construct(
 			bspump.socket.TCPStreamSource(app, self),
-			bspump.amqp.AMQPSink(app, self, self.driver)
+			bspump.amqp.AMQPSink(app, self, driver)
 		)
-
 
 class SamplePipeline2(bspump.Pipeline):
 
 	def __init__(self, app, pipeline_id, driver):
 		super().__init__(app, pipeline_id)
-
-		self.driver = driver
-
 		self.construct(
-			bspump.amqp.AMQPSource(app, self, self.driver),
+			bspump.amqp.AMQPSource(app, self, driver),
 			bspump.common.PPrintSink(app, self)
 		)
-
 
 if __name__ == '__main__':
 	app = bspump.BSPumpApplication()
