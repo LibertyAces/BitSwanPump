d82539f3d655fca71028af8a331343598eb6bf44
Miloslav Pavelka <miloslav.pavelka@teskalabs.com>
2018-03-14 17:31:07 +0100

    Adding file source.

---
+++ bspump/file/__init__.py
@@ -0,0 +1,1 @@
+from .filesource import FileSource
---
+++ bspump/file/filesource.py
@@ -0,0 +1,36 @@
+import logging
+import asyncio
+from .. import Source
+
+#
+
+L = logging.getLogger(__file__)
+
+#
+
+
+class FileSource(Source):
+
+	ConfigDefaults = {
+		'path': '',
+	}
+
+	def __init__(self, app, pipeline, id=None, config=None):
+		super().__init__(app, pipeline, id=id, config=config)
+
+		self.Loop = app.Loop
+
+	async def process_file_coro(self):
+		async def process_line_coro(line):
+			self.process(line)
+
+		try:
+			with open(self.Config['path'], "rb") as f:
+				for line in f:
+					await asyncio.ensure_future(process_line_coro(line), loop=self.Loop)
+		except IOError:
+			L.error("The specified file {} could not be opened.".format(self.Config['path']))
+
+	async def start(self):
+		# Run the reading of the file
+		asyncio.ensure_future(self.process_file_coro(), loop=self.Loop)
