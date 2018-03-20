c099dbdfc5537400c6790e9bd84b4b933b09eb5f
Ales Teska <ales.teska@teskalabs.com>
2018-03-20 19:36:33 +0100

    Add is_file_open() check (using lsof).

--- bspump/file/globscan.py
+++ bspump/file/globscan.py
@@ -1,5 +1,16 @@
 import glob
 import os.path
+import subprocess
+import platform
+
+if platform.system() == "Windows":
+	def _is_file_open(fname):
+		#TODO: Provide implementation of _is_file_open() for Windows
+		return False
+else:
+	def _is_file_open(fname):
+		result = subprocess.run(['lsof', fname], stdout=subprocess.PIPE)
+		return len(result.stdout) != 0
 
 def _glob_scan(path):
 	if path is None: return None
@@ -12,8 +23,7 @@ def _glob_scan(path):
 		if fname.endswith('-locked'): continue
 		if fname.endswith('-processed'): continue
 		if not os.path.isfile(fname): continue
-
-		#TODO: Validate thru lsof ...
+		if _is_file_open(fname): continue
 
 		return fname
 
