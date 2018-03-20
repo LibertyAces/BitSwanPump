77eed86edd788e12b80a750a21e44cb4536b5236
Přemysl Černý <premysl.cerny@teskalabs.com>
2018-03-20 12:29:31 +0100

    Fixing FileLineSource modes

--- bspump/file/filesource.py
+++ bspump/file/filesource.py
@@ -32,26 +32,27 @@ class FileLineSource(Source):
 		try:
 			if filename.endswith(".gz"):
 				import gzip
-				f = gzip.open(filename, 'rb')
+				f = gzip.open(filename, mode)
 
 			elif filename.endswith(".bz2"):
 				import bz2
-				f = bz2.open(filename, 'rb')
+				f = bz2.open(filename, mode)
 
 			elif filename.endswith(".xz") or filename.endswith(".lzma"):
 				import lzma
-				f = lzma.open(filename, 'rb')
+				f = lzma.open(filename, mode)
 			else:
-				f = open(filename, 'rb')
+				f = open(filename, mode)
 
 		except IOError:
 			L.error("The specified file {} could not be opened.".format(self.Config['path']))
-		
-		for line in f:
-			await self.Pipeline.ready()
-			self.process(line)
 
-		f.close()
+		try:
+			for line in f:
+				await self.Pipeline.ready()
+				self.process(line)
+		finally:
+			f.close()
 
 	async def start(self):
 		self._future = asyncio.ensure_future(self._read_file(), loop=self.Loop)
