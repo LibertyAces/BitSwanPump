7f0189bac9894f46ff161ee86bbc814c59e4b85b
Ales Teska <ales.teska@teskalabs.com>
2018-03-22 00:30:13 +0100

    Add default naming to Sources & Processors.

--- bspump/abcproc.py
+++ bspump/abcproc.py
@@ -7,7 +7,7 @@ class ProcessorBase(abc.ABC, ConfigObject):
 	def __init__(self, app, pipeline, id=None, config=None):
 		super().__init__("pipeline:{}:{}".format(pipeline.Id, id if id is not None else self.__class__.__name__), config=config)
 
-		self.Id = id
+		self.Id = id if id is not None else self.__class__.__name__
 		self.Pipeline = pipeline
 
 
@@ -36,7 +36,7 @@ class Source(abc.ABC, ConfigObject):
 	def __init__(self, app, pipeline, id=None, config=None):
 		super().__init__("pipeline:{}:{}".format(pipeline.Id, id if id is not None else self.__class__.__name__), config=config)
 
-		self.Id = id
+		self.Id = id if id is not None else self.__class__.__name__
 		self.Pipeline = pipeline
 
 
