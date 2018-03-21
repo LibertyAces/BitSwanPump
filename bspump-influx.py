71059322386a9c630a998e526d88538b41521a5d
Jan Pišl <jan.pisl@teskalabs.com>
2018-03-21 18:07:36 +0100

    Sample pipeline added for testing InfluxDB Sink.

---
+++ bspump-influx.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import logging
+import asyncio
+import asab
+import bspump
+import bspump.socket
+import bspump.common
+import bspump.influxdb
+
+###
+
+L = logging.getLogger(__name__)
+
+###
+
+class SamplePipeline(bspump.Pipeline):
+
+	'''
+	Test this pipeline by
+	$ echo '{"Ahoj":"svete"}' |  nc localhost 7000
+	'''
+
+	def __init__(self, app, pipeline_id):
+		super().__init__(app, pipeline_id)
+		self.build(
+			bspump.socket.TCPStreamSource(app, self, config={'port': 7000}),
+			bspump.influxdb.InfluxDBSink(app, self, "InfluxConnection1")
+		)
+
+
+if __name__ == '__main__':
+	app = bspump.BSPumpApplication()
+
+	svc = app.get_service("bspump.PumpService")
+
+	svc.add_connection(
+		bspump.influxdb.InfluxDBConnection(app, "InfluxConnection1")
+	)
+
+	# Construct and register Pipeline
+	pl = SamplePipeline(app, 'SamplePipeline')
+	svc.add_pipeline(pl)
+
+	app.run()
