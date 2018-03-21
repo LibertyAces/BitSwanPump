13377bc6e9fba4c7b964a6f574ce999bb721541f
Jan Pišl <jan.pisl@teskalabs.com>
2018-03-21 18:07:03 +0100

    InfluxDB Sink divided into two files (connection and sink), more functionality added.

---
+++ bspump/influxdb/sink.py
@@ -0,0 +1,64 @@
+import logging
+import asyncio
+import aiohttp
+
+import asab
+import bspump
+
+from .. import Sink
+
+#
+
+L = logging.getLogger(__name__)
+
+#
+
+class InfluxDBSink(Sink):
+
+	def __init__(self, app, pipeline, connection, id=None, config=None):
+		super().__init__(app, pipeline, id=id, config=config)
+		self._connection = pipeline.locate_connection(app, connection)
+
+		app.PubSub.subscribe("InfluxDBConnection.pause!", self._connection_throttle)
+		app.PubSub.subscribe("InfluxDBConnection.unpause!", self._connection_throttle)		
+
+
+	def process(self, line):
+		{
+			"measurement" : "cpu_load_short",
+			"tag_set": "ads=fd,gd=dfs",
+			"field_set": "value=0.64",
+			"timestamp": "1434055562000000000"
+		}
+		
+		# Getting information from the data
+		# Working with only one line
+
+		if isinstance(line, tuple):
+			measurement, tag_set, field_set, timestamp = line
+			wire_line = "{},{} {} {}\n".format(measurement, tag_set, field_set, int(timestamp * 1e9))
+
+		elif isinstance(line, bytes):
+			measurement = 'line'
+			wire_line = line.decode('utf-8')
+			if wire_line[-1:] != '\n': wire_line += '\n'
+
+		print("#######")
+		# Passing the processed line to the connection
+		self._connection.consume(wire_line)
+
+
+	def flush(self):
+		self._connection.flush()
+
+
+	def _connection_throttle(self, event_name, connection):
+		if connection != self._connection:
+			return
+
+		if event_name == "InfluxDBConnection.pause!":
+			self.Pipeline.throttle(self, True)
+		elif event_name == "InfluxdDBConnection.unpause!":
+			self.Pipeline.throttle(self, False)
+		else:
+			raise RuntimeError("Unexpected event name '{}'".format(event_name))
