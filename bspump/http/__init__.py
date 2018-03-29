f2faf6a74399d0e4c507e01113622ce9e9555de1
Ales Teska <ales.teska@teskalabs.com>
2018-03-29 18:47:50 +0200

    Add a HTTP client source.

---
+++ bspump/http/__init__.py
@@ -0,0 +1,3 @@
+from .client.source import HTTPClientSource
+from .client.source import HTTPClientTextSource
+from .client.source import HTTPClientLineSource
---
+++ bspump/http/client/source.py
@@ -0,0 +1,38 @@
+from . abcsource import HTTPABCClientSource
+
+###
+
+class HTTPClientSource(HTTPABCClientSource):
+
+	async def read(self, response):
+		response = await response.read()
+		self.process(response)
+
+###
+
+class HTTPClientTextSource(HTTPABCClientSource):
+
+	ConfigDefaults = {
+		'encoding': '',
+	}
+
+	def __init__(self, app, pipeline, id=None, config=None):
+		super().__init__(app, pipeline, id=id, config=config)
+
+		self.encoding = self.Config['encoding']
+		if self.encoding == '': self.encoding = None
+
+	async def read(self, response):
+		response = await response.text(encoding=self.encoding)
+		self.process(response)
+
+###
+
+class HTTPClientLineSource(HTTPClientTextSource):
+
+
+	async def read(self, response):
+		response = await response.text(encoding=self.encoding)
+
+		for line in response.split('\n'):
+			self.process(line)
