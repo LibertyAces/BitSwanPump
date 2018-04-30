98dd7baaa60f9e73640d85c2f05058c27c3116d0
Jan Pišl <jan.pisl@teskalabs.com>
2018-04-30 17:13:48 +0200

    Introduce FileBlockSink that writes data to a file.

--- bspump/file/__init__.py
+++ bspump/file/__init__.py
@@ -1,3 +1,4 @@
 from .filelinesource import FileLineSource
 from .filelinesource import FileMultiLineSource
 from .fileblocksource import FileBlockSource
+from .fileblocksink import FileBlockSink
---
+++ bspump/file/fileblocksink.py
@@ -0,0 +1,27 @@
+import logging
+from ..abc.sink import Sink
+
+#
+
+L = logging.getLogger(__file__)
+
+#
+
+class FileBlockSink(Sink):
+
+	ConfigDefaults = {
+		'filename': '',
+	}
+
+
+	def get_file_name(self, context, event):
+		'''
+		Override this method to gain control over output file name.
+		'''
+		return self.Config['filename']
+
+
+	def process(self, context, event):
+		fname = self.get_file_name(context, event)
+		with open(fname, 'wb') as fo:
+			fo.write(event)
