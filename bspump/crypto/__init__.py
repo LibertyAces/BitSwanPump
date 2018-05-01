21c3f26796af1c5d68cb8ef328f4fd8328030eec
Ales Teska <ales.teska@teskalabs.com>
2018-05-01 23:36:09 +0200

    Switch to cryptography from PyCrypto (#24)
    Chage of the cryptographic library (from pycrypto to cryptographic).

--- bspump/crypto/__init__.py
+++ bspump/crypto/__init__.py
@@ -1,9 +1,9 @@
-from .aes import DecryptAESProcessor
 from .aes import EncryptAESProcessor
+from .aes import DecryptAESProcessor
 
 '''
 Test AES
 
-openssl aes-128-cbc -e -in /etc/services -out /Users/ateska/Downloads/services.enc -K 00000000000000000000000000000000 -iv 00000000000000000000000000000000 
+openssl aes-128-cbc -e -in /etc/services -K 00000000000000000000000000000000 -iv 00000000000000000000000000000000
 
 '''
--- bspump/crypto/aes.py
+++ bspump/crypto/aes.py
@@ -1,69 +1,32 @@
 import binascii
-from Crypto.Cipher import AES
+
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
 from ..abc.processor import Processor
-from .padding import PKCS7Padding, NoPadding
 
 #
 
 class CommonAESMixin(object):
 
+
 	ConfigDefaults = {
 		"key" : "00000000000000000000000000000000", # Hexadecimal key, It must be 16 (AES-128), 24 (AES-192), or 32 (AES-256) bytes long.
 		"iv": "00000000000000000000000000000000",
-		"mode": "CBC", # One of ECB (Electronic Code Book), CBC (Cipher-Block Chaining), CFB (Cipher FeedBack), OFB (Output FeedBack), CTR (CounTer Mode), OPENPGP (OpenPGP Mode)
-		"padding": "PKCS7", # One of 'PKCS7', 'None'
 	}
 
 
 	def initalize_aes(self):
-		self.Key = binascii.unhexlify(self.Config['key'])
-		self.IV = binascii.unhexlify(self.Config['iv'])
-		
-		self.Mode = {
-			'ECB': AES.MODE_ECB,
-			'CBC': AES.MODE_CBC,
-			'CFB': AES.MODE_CFB,
-			'OFB': AES.MODE_OFB,
-			'CTR': AES.MODE_CTR,
-			'OPENPGP': AES.MODE_OPENPGP,
-		}.get(self.Config['mode'].upper())
-		if self.Mode is None:
-			L.error("Unknown AES mode '{}'".format(self.Config['mode']))
-			raise RuntimeError("Unknown AES mode")
+		backend = default_backend()
 
-		self.Padding = {
-			'None': NoPadding,
-			'PKCS7': PKCS7Padding,
-		}.get(self.Config['padding'].upper())(AES.block_size, self.Config)
-		if self.Padding is None:
-			L.error("Unknown padding '{}'".format(self.Config['padding']))
-			raise RuntimeError("Unknown padding")
+		key = binascii.unhexlify(self.Config['key'])
+		iv = binascii.unhexlify(self.Config['iv'])
+
+		self.padding = padding.PKCS7(128)
+		self.cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
 
 
-	def cipher(self, context):
-		'''
-		Override this method for a custom AES cipher configuration
-		'''
-		iv = context.get('iv', self.IV)
-		return AES.new(self.Key, self.Mode, iv)
-
-#
-
-class DecryptAESProcessor(Processor, CommonAESMixin):
-
-	def __init__(self, app, pipeline, id=None, config=None):
-		super().__init__(app, pipeline, id, config)
-		self.initalize_aes()
-
-
-	def process(self, context, event):
-		cipher = self.cipher(context)
-		event = cipher.decrypt(event)
-		event = self.Padding.decode(event)
-		return event
-
-#
 
 class EncryptAESProcessor(Processor, CommonAESMixin):
 
@@ -74,7 +37,24 @@ class EncryptAESProcessor(Processor, CommonAESMixin):
 
 
 	def process(self, context, event):
-		cipher = self.cipher(context)
-		event = self.Padding.encode(event)
-		event = cipher.encrypt(event)
-		return event
+		encryptor = self.cipher.encryptor()
+		padder = self.padding.padder()
+		data = padder.update(event) + padder.finalize()
+		return encryptor.update(data) + encryptor.finalize()
+
+#
+
+class DecryptAESProcessor(Processor, CommonAESMixin):
+
+
+	def __init__(self, app, pipeline, id=None, config=None):
+		super().__init__(app, pipeline, id, config)
+		self.initalize_aes()
+
+
+	def process(self, context, event):
+		decryptor = self.cipher.decryptor()
+		unpadder = self.padding.unpadder()
+		data = decryptor.update(event) + decryptor.finalize()
+		return unpadder.update(data) + unpadder.finalize()
+
