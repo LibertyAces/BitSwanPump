6065f0f3d787d69f93ca9266c1fbe00b240f781b
Ales Teska <ales.teska@teskalabs.com>
2018-05-29 10:10:05 +0200

    Fix of the typo.

--- bspump/abc/connection.py
+++ bspump/abc/connection.py
@@ -4,5 +4,5 @@ from .config import ConfigObject
 class Connection(abc.ABC, ConfigObject):
 
 	def __init__(self, app, connection_id, config=None):
-		super().__init__("connection:{}".format(connection_id), config=None)
+		super().__init__("connection:{}".format(connection_id), config=config)
 		self.Id = connection_id
