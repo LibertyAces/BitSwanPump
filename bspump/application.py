be7a58c62a731ae8d985a3299214b679e341b4da
Miloslav Pavelka <pavelkamiloslav@gmail.com>
2018-08-10 13:29:49 +0200

    Activate web service from configuration. (#38)
    * Activate web service from configuration.

--- bspump/application.py
+++ bspump/application.py
@@ -2,17 +2,22 @@ import asab
 
 from .service import BSPumpService
 
+
 class BSPumpApplication(asab.Application):
 
-	def __init__(self, web=False):
+	def __init__(self):
 		super().__init__()
 
 		self.PumpService = BSPumpService(self)
 
 		# Conditionally activate also a web service
-		if web:
-			from .web import initialize_web
-			self.WebService = initialize_web(self)
+		if asab.Config.has_section("asab:web"):
+			listen = asab.Config["asab:web"].get("listen", "")
+			if len(listen) > 0:
+				# Initialize web service
+				from .web import initialize_web
+				self.WebService = initialize_web(self)
+
 		else:
 			self.WebService = None
 
