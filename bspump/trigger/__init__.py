e4cc03b833b746ba701bba366df5431f3a3be478
Ales Teska <ales.teska@teskalabs.com>
2018-10-25 18:04:36 +0200

    Add a periodic trigger.

--- bspump/trigger/__init__.py
+++ bspump/trigger/__init__.py
@@ -2,3 +2,4 @@ from .trigger import Trigger
 from .opportunistic import OpportunisticTrigger
 from .runonce import RunOnceTrigger
 from .pubsub import PubSubTrigger
+from .periodic import PeriodicTrigger
---
+++ bspump/trigger/periodic.py
@@ -0,0 +1,20 @@
+import asab
+from .trigger import Trigger
+
+###
+
+class PeriodicTrigger(Trigger):
+	
+
+	def __init__(self, app, interval=None, id=None):
+		'''
+		Interval is in seconds, can be a float or int.
+		'''
+
+		super().__init__(app, id)
+		self.Timer = asab.Timer(app, self.on_timer, autorestart=True)
+		self.Timer.start(interval)
+
+
+	async def on_timer(self):
+		self.fire()
