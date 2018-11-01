03d91f3b565127c3cd23564f96d1662c94e59320
Přemysl Černý <premysl.cerny@teskalabs.com>
2018-11-01 11:10:32 +0100

    Adding assert.

--- bspump/web/__init__.py
+++ bspump/web/__init__.py
@@ -43,6 +43,8 @@ async def lookup(request):
 	except AttributeError:
 		raise aiohttp.web.HTTPNotImplemented()
 
+	assert(isinstance(data, bytes))
+
 	response_etag = hashlib.sha1(data).hexdigest()
 	if request_etag == response_etag:
 		raise aiohttp.web.HTTPNotModified()
