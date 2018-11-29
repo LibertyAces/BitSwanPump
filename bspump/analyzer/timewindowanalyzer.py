2c31c92cc7a87c760bd07f9a9c423ad15f9cdb5e
margirova <44573186+margirova@users.noreply.github.com>
2018-11-29 17:45:37 +0100

    Units merge conflict solved

--- bspump/analyzer/timewindowanalyzer.py
+++ bspump/analyzer/timewindowanalyzer.py
@@ -39,9 +39,9 @@ class TimeWindow(object):
 	def __init__(self, app, pipeline, start_time, resolution=60, columns=15):
 
 		if start_time is None:
-			start_time = time.time() * 1000
+			start_time = time.time()
 
-		self.Resolution = resolution * 1000
+		self.Resolution = resolution
 		self.Columns = columns
 
 		self.Start = (1 + (start_time // self.Resolution)) * self.Resolution
@@ -202,7 +202,7 @@ class TimeWindowAnalyzer(Analyzer):
 
 	async def _on_tick(self):
 
-		target_ts = time.time() * 1000
+		target_ts = time.time()
 		self.advance(target_ts)
 #		if not self.WarmingUp:
 #			await self.analyze()
