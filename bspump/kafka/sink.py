7a7ee47492dac3d319bb3f0e6bc3c783e29b15e5
Ales Teska <ales.teska@teskalabs.com>
2018-12-10 07:34:27 +0100

    Name refactoring in the Kafka source.

--- bspump/kafka/sink.py
+++ bspump/kafka/sink.py
@@ -21,19 +21,19 @@ class KafkaSink(Sink):
 	def __init__(self, app, pipeline, connection, id=None, config=None):
 		super().__init__(app, pipeline, id=id, config=config)
 
-		self._connection = pipeline.locate_connection(app, connection)
-		self._topic = self.Config['topic']
+		self.Connection = pipeline.locate_connection(app, connection)
+		self.Topic = self.Config['topic']
 
-		app.PubSub.subscribe("KafkaConnection.pause!", self._connection_throttle)
-		app.PubSub.subscribe("KafkaConnection.unpause!", self._connection_throttle)
+		app.PubSub.subscribe("KafkaConnection.pause!", self.Connection_throttle)
+		app.PubSub.subscribe("KafkaConnection.unpause!", self.Connection_throttle)
 
 
 	def process(self, context, event):
-		self._connection.consume(self._topic, event)
+		self.Connection.consume(self.Topic, event)
 
 
 	def _connection_throttle(self, event_name, connection):
-		if connection != self._connection:
+		if connection != self.Connection:
 			return
 
 		if event_name == "KafkaConnection.pause!":
