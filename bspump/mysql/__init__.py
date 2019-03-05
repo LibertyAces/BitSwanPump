bedb32f97815eeed982db7b09786f3f58403bbb3
margirova <margarita.argirova@teskalabs.com>
2019-03-05 15:57:38 +0100

    added binlog source

--- bspump/mysql/__init__.py
+++ bspump/mysql/__init__.py
@@ -1,4 +1,4 @@
-from .connection import MySQLConnection
+from .connection import MySQLConnection, MySQLBinaryLogSource
 from .source import MySQLSource
 from .sink import MySQLSink
 from .lookup import MySQLLookup
--- bspump/mysql/source.py
+++ bspump/mysql/source.py
@@ -3,6 +3,7 @@ import asyncio
 import logging
 import aiomysql.cursors
 from ..abc.source import TriggerSource
+import pymysqlreplication
 
 #
 
@@ -46,3 +47,86 @@ class MySQLSource(TriggerSource):
 					await cur.execute("COMMIT;")
 			except BaseException as e:
 				L.exception("Unexpected error when processing MySQL query.")
+
+
+class MySQLBinaryLogSource(TriggerSource):
+
+	ConfigDefaults = {
+		'server_id': 1
+	}
+	
+	def __init__(self, app, pipeline, connection, id=None, config=None):
+		super().__init__(app, pipeline, id=id, config=config)
+		self._connection = pipeline.locate_connection(app, connection)
+		self.MySQLSettings = {
+			'host': self._connection._host, 
+			'port': self._connection._port, 
+			'user': self._connection._user, 
+			'passwd': self._connection._password, 
+		}
+		self.ServerId = int(self.Config['server_id'])
+		self.Stream = pymysqlreplication.BinLogStreamReader(connection_settings=self.MySQLSettings, server_id=self.ServerId)
+
+
+	async def cycle(self):
+		await self.Pipeline.ready()
+		for binlogevent in self.Stream:
+			event = {}
+
+			# General info
+			event_type = binlogevent.__class__.__name__
+			event['type'] = event_type
+			event['@timestamp'] = binlogevent.timestamp
+			event['log_position'] = binlogevent.packet.log_pos
+			event['event_size'] = binlogevent.event_size
+			event['read_bytes'] = binlogevent.packet.read_bytes
+
+			# Specific info
+			if event_type in ['DeleteRowsEvent', 'WriteRowsEvent', 'UpdateRowsEvent']:
+				event['rows'] = binlogevent.rows
+			
+			if event_type == 'TableMapEvent':
+				event['table_id'] = binlogevent.table_id
+				event['schema'] = binlogevent.schema
+				event['table'] = binlogevent.table
+				event['columns'] = binlogevent.column_count
+
+			if event_type == 'GtidEvent':
+				event['commit'] = binlogevent.commit_flag
+				event['GTID_NEXT'] = binlogevent.gtid
+
+			if event_type == 'RotateEvent':
+				event['position'] = binlogevent.position
+				event['next_binlog_file'] = binlogevent.next_binlog
+				
+			if event_type == 'XidEvent':
+				event['transaction_id'] = binlogevent.xid
+			
+			if event_type == 'HeartbeatLogEvent':
+				event['current_binlog'] = binlogevent.ident
+
+			if event_type == 'QueryEvent':
+				event['schema'] = binlogevent.schema
+				event['execution_time'] = binlogevent.execution_time
+				event['query'] = binlogevent.query
+
+			if event_type == 'BeginLoadQueryEvent':
+				event['file_id'] = binlogevent.file_id
+				event['block_data'] = binlogevent.block_data
+
+			if event_type == 'ExecuteLoadQueryEvent':
+				event['slave_proxy_id'] = binlogevent.slave_proxy_id
+				event['execution_time'] = binlogevent.execution_time
+				event['schema_length'] = binlogevent.schema_length
+				event['error_code'] = binlogevent.error_code
+				event['status_vars_length'] = binlogevent.status_vars_length
+				event['file_id'] = binlogevent.file_id
+				event['start_pos'] = binlogevent.start_pos
+				event['end_pos'] = binlogevent.end_pos
+				event['dup_handling_flags'] = binlogevent.dup_handling_flags
+
+			if event_type == 'IntvarEvent':
+				event['type'] = binlogevent.type
+				event['value'] = binlogevent.value
+
+			await self.process(event)
