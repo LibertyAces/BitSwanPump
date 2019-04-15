4d06185b9ff5c58b64d75bd450e13949a3f7ab07
margirova <margarita.argirova@teskalabs.com>
2019-04-15 16:30:30 +0200

    moved geoanalyzer

--- bspump/analyzer/__init__.py
+++ bspump/analyzer/__init__.py
@@ -2,4 +2,6 @@ from .analyzer import Analyzer
 from .timewindowanalyzer import TimeWindowAnalyzer
 from .timedriftanalyzer import TimeDriftAnalyzer
 from .sessionanalyzer import SessionAnalyzer
+from .geoanalyzer import GeoAnalyzer
 from .matrixcontainer import MatrixContainer, TimeWindowMatrixContainer, SessionMatrixContainer
+from .geomatrixcontainer import GeoMatrixContainer
---
+++ bspump/analyzer/geoanalyzer.py
@@ -0,0 +1,49 @@
+import logging
+import time
+import asab
+
+import numpy as np
+
+from .analyzer import Analyzer, MatrixContainer
+from .geomatrixcontainer import GeoMatrixContainer 
+
+
+##
+
+L = logging.getLogger(__name__)
+
+##
+
+class GeoAnalyzer(Analyzer):
+	
+	ConfigDefaults = {
+		"resolution": 5,  # 5 km in one cell
+		"max_lat": 71.26,  # Europe endpoints
+		"min_lat": 23.33,
+		"min_lon": -10.10,
+		"max_lon": 40.6,
+		"analyze_period": 60*60, # for now
+	}
+
+	def __init__(self, app, pipeline, container=None, id=None, config=None):
+		super().__init__(app, pipeline, id=id, config=config)
+		
+		if container is None:
+			bbox = {
+				"min_lat": float(self.ConfigDefaults["min_lat"]),
+				"max_lat": float(self.ConfigDefaults["max_lat"]),
+				"min_lon": float(self.ConfigDefaults["min_lon"]),
+				"max_lon": float(self.ConfigDefaults["max_lon"]),
+			}
+			self.GeoMatrixContainer = GeoMatrixContainer(app, pipeline, bbox, resolution=5)
+		else:
+			self.GeoMatrixContainer = container
+		
+		self.Matrix = self.GeoMatrixContainer.Matrix['geo_matrix']  # alias
+		self.AnalyzePeriod = int(self.Config['analyze_period'])
+		self.AnalyzeTimer = asab.Timer(app, self._on_tick_analyze, autorestart=True)
+		self.AnalyzeTimer.start(self.AnalyzePeriod)
+
+
+	async def _on_tick_analyze(self):
+		await self.analyze()
