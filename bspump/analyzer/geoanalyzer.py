769dea496a08021b99c1c12a34edae7f9621ca0a
Margarita Argirova <margarita.argirova@teskalabs.com>
2019-04-17 10:09:01 +0000

    Update geoanalyzer.py

--- bspump/analyzer/geoanalyzer.py
+++ bspump/analyzer/geoanalyzer.py
@@ -22,7 +22,6 @@ class GeoAnalyzer(Analyzer):
 		"min_lat": 23.33,
 		"min_lon": -10.10,
 		"max_lon": 40.6,
-		"analyze_period": 60*60, # for now
 	}
 
 	def __init__(self, app, pipeline, container=None, id=None, config=None):
@@ -40,10 +39,3 @@ class GeoAnalyzer(Analyzer):
 			self.GeoMatrixContainer = container
 		
 		self.Matrix = self.GeoMatrixContainer.Matrix['geo_matrix']  # alias
-		self.AnalyzePeriod = int(self.Config['analyze_period'])
-		self.AnalyzeTimer = asab.Timer(app, self._on_tick_analyze, autorestart=True)
-		self.AnalyzeTimer.start(self.AnalyzePeriod)
-
-
-	async def _on_tick_analyze(self):
-		await self.analyze()
