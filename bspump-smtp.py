87bfdcb9164a5f75db303066ecd0c60affd37ee4
Adam Wichera <adam.wichera@teskalabs.com>
2019-04-26 17:35:02 +0200

    Changes config passing to comply with add_defaults pattern.

--- bspump-smtp.py
+++ bspump-smtp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging
+import asab
 import bspump
 import bspump.file
 import bspump.common
@@ -15,38 +16,46 @@ L = logging.getLogger(__name__)
 
 # python -m smtpd -c DebuggingServer -n localhost:1025
 
+asab.Config.add_defaults(
+	{
+		'connection:SmtpConnection1': {
+			"smtp_server": "localhost",
+			"port": 1025,
+			"from": "my@email.com",
+			"to": "your@email.com,his@email.com",
+			"cc": "her@email.com",
+			"bcc": "its@email.com",
+			"subject": "BSPump mailing service",
+			"output_queue_max_size": 10
+		},
+		'pipeline:SamplePipeline:FileLineSource1':{
+			'path' : 'data.txt', # provide source file to run the pump
+			'post': 'noop' # source file won't be renamed
+		}
+	}
+)
+
 class SamplePipeline(bspump.Pipeline):
 
 	def __init__(self, app, pipeline_id):
 		super().__init__(app, pipeline_id)
 
 		self.build(
-			bspump.file.FileLineSource(app, self, config={'path': 'data.txt', 'post':'noop'}).on(bspump.trigger.RunOnceTrigger(app)),
+			bspump.file.FileLineSource(app, self,'FileLineSource1').on(bspump.trigger.RunOnceTrigger(app)),
 			bspump.common.BytesToStringParser (app, self),
 			bspump.common.PPrintProcessor(app, self),
-			bspump.mail.SmtpSink(app, self, 'SmptConnection')
+			bspump.mail.SmtpSink(app, self, 'SmtpConnection1')
 		)
-
-
-smtp_connection_config = {
-	"smtp_server": "localhost",
-	"port": 1025,
-	"from": "my@email.com",
-	"to": "your@email.com,his@email.com",
-	"cc": "her@email.com",
-	"bcc": "its@email.com",
-	"subject": "SMTPSink Test mail",
-	"output_queue_max_size": 10
-}
 
 
 if __name__ == '__main__':
 	app = bspump.BSPumpApplication()
 
+
 	svc = app.get_service("bspump.PumpService")
 
 	svc.add_connection(
-		bspump.mail.SmtpConnection(app, "SmptConnection", config = smtp_connection_config)
+		bspump.mail.SmtpConnection(app, "SmtpConnection1")
 	)
 
 	# Construct and register Pipeline
