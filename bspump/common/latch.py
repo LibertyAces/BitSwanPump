b81a066fa6820d20f299ba4b6f6a51cd729697d9
Jakub Boukal <www.bagr@gmail.com>
2019-05-22 17:35:53 +0200

    Add doc string to LatchProcessor

--- bspump/common/latch.py
+++ bspump/common/latch.py
@@ -3,6 +3,13 @@ import collections
 
 
 class LatchProcessor(Processor):
+	"""
+		Latch accumulates events in the queue of maximum specified size - `queue_max_size`
+
+		If `queue_max_size` is 0 then queue is not limited
+
+		If accumulated events exceeds `queue_max_size` then first event is dropped
+	"""
 
 	ConfigDefaults = {
 		'queue_max_size': 50,  # 0 means unlimited size
