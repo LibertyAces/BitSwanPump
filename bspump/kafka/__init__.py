0d95e38f8c152544610d44f1f8de95dce3e711a9
Adam Wichera <adam.wichera@teskalabs.com>
2019-05-30 17:05:58 +0200

    Removes KafkaMultiSink from kafka module __init__ .

--- bspump/kafka/__init__.py
+++ bspump/kafka/__init__.py
@@ -1,11 +1,9 @@
 from .connection import KafkaConnection
 from .source import KafkaSource
 from .sink import KafkaSink
-from .sink import KafkaMultiSink
 
 __all__ = [
 	"KafkaConnection",
 	"KafkaSource",
 	"KafkaSink",
-	"KafkaMultiSink",
 ]
