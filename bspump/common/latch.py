540a94e07879427953e24a51c7bedf9747589c26
margirova <margarita.argirova@teskalabs.com>
2019-06-18 10:18:05 +0200

    quue is public, deleted list_queue

--- bspump/common/latch.py
+++ bspump/common/latch.py
@@ -9,7 +9,14 @@ class LatchProcessor(Processor):
 
 		If `queue_max_size` is 0 then queue is not limited
 
-		If accumulated events exceeds `queue_max_size` then first event is dropped
+		If accumulated events exceeds `queue_max_size` then first event is dropped.
+
+		The queue can be filled based on the query (empty by default). The query is mongo-like,
+		see the rules in `ContentFilter`. If inclusive is True (default), matched with the query event is 
+		added to the queue, otherwise skipped.
+
+		The query may be injected with an API call to allow to control events in the queue.
+		 
 	"""
 
 	ConfigDefaults = {
@@ -17,15 +24,15 @@ class LatchProcessor(Processor):
 	}
 
 	def __init__(self, app, pipeline, id=None, config=None):
-		super().__init__(app, pipeline, query={}, id=id, config=config)
+		super().__init__(app, pipeline, query={}, inclusive=True, id=id, config=config)
+		self.Inclusive = inclusive
 		max_size = int(self.Config.get('queue_max_size'))
 		if max_size == 0:
-			self._Queue = collections.deque()
+			self.Queue = collections.deque()
 		else:
-			self._Queue = collections.deque(maxlen=max_size)
+			self.Queue = collections.deque(maxlen=max_size)
 
-		# Check if the query is correctly implemented
-		
+		# Check if the query is correctly implemented		
 		try:
 			self.Query = mongoquery.Query(query)
 			self.Query.match({})
@@ -35,18 +42,8 @@ class LatchProcessor(Processor):
 		
 
 	def process(self, context, event):
-		if self.Query.match(event):
-			self._Queue.append(event)
+		if self.Query.match(event) == self.Inclusive:
+			self.Queue.append(event)
 		return event
 
-
-	def list_queue(self, query=None):
-		if query is not None:
-			try:
-				self.Query = mongoquery.Query(query)
-				self.Query.match({})
-			except mongoquery.QueryError:
-				L.warn("Incorrect query")
-				raise
-		return list(self._Queue)
 
