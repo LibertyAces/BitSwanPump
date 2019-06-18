df32bad6d8501c2a5d0365f51e26e62eb8e99781
margirova <margarita.argirova@teskalabs.com>
2019-06-18 10:21:12 +0200

    queue => latch

--- bspump/common/latch.py
+++ bspump/common/latch.py
@@ -5,32 +5,32 @@ import mongoquery
 
 class LatchProcessor(Processor):
 	"""
-		Latch accumulates events in the queue of maximum specified size - `queue_max_size`
+		Latch accumulates events in the Latch of maximum specified size - `latch_max_size`
 
-		If `queue_max_size` is 0 then queue is not limited
+		If `latch_max_size` is 0 then latch is not limited
 
-		If accumulated events exceeds `queue_max_size` then first event is dropped.
+		If accumulated events exceeds `latch_max_size` then first event is dropped.
 
-		The queue can be filled based on the query (empty by default). The query is mongo-like,
+		The latch can be filled based on the query (empty by default). The query is mongo-like,
 		see the rules in `ContentFilter`. If inclusive is True (default), matched with the query event is 
-		added to the queue, otherwise skipped.
+		added to the latch, otherwise skipped.
 
-		The query may be injected with an API call to allow to control events in the queue.
-		 
+		The query can be injected with an API call to allow to control events in the latch.
+
 	"""
 
 	ConfigDefaults = {
-		'queue_max_size': 50,  # 0 means unlimited size
+		'latch_max_size': 50,  # 0 means unlimited size
 	}
 
 	def __init__(self, app, pipeline, id=None, config=None):
 		super().__init__(app, pipeline, query={}, inclusive=True, id=id, config=config)
 		self.Inclusive = inclusive
-		max_size = int(self.Config.get('queue_max_size'))
+		max_size = int(self.Config.get('latch_max_size'))
 		if max_size == 0:
-			self.Queue = collections.deque()
+			self.Latch = collections.deque()
 		else:
-			self.Queue = collections.deque(maxlen=max_size)
+			self.Latch = collections.deque(maxlen=max_size)
 
 		# Check if the query is correctly implemented		
 		try:
@@ -43,7 +43,7 @@ class LatchProcessor(Processor):
 
 	def process(self, context, event):
 		if self.Query.match(event) == self.Inclusive:
-			self.Queue.append(event)
+			self.Latch.append(event)
 		return event
 
 
