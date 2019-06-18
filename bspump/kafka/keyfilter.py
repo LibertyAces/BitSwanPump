3704364992afea5491da8df57847b73f5e7dbe9b
Adam Wichera <adam.wichera@teskalabs.com>
2019-06-18 13:34:20 +0200

    Refactors KafkaKeyFilter using frozenset.

--- bspump/kafka/keyfilter.py
+++ bspump/kafka/keyfilter.py
@@ -7,62 +7,27 @@ class KafkaKeyFilter(bspump.Processor):
 	key provided in each event.
 
 	Every Kafka message has a key, KafkaKeyFilter selects only those events where
-	the key matches selected "filter_key", other events will be discarded. You may set filtering `key`
+	the event key matches one of provided 'keys', other events will be discarded. You may set filtering `keys`
 	as a processor parameter (bytes).
 
-	KafkaKeyFilter	 is meant to be inserted after KafkaSource in a Pipeline.
+	KafkaKeyFilter is meant to be inserted after KafkaSource in a Pipeline.
 	"""
 
 	def __init__(self, app, pipeline, keys, id=None, config=None):
 		super().__init__(app, pipeline, id, config)
-		if isinstance (keys,list):
-			self._keys = keys
-			self.process = self.process_keys
+		if not isinstance (keys,list):
+			self.Keys = frozenset([keys])
 		else:
-			self._key = keys
-			self.process = self.process_key
+			self.Keys = frozenset(keys)
 
-	def process (self, context, event):
-		pass
 
-	def process_key(self, context, event):
+
+	def process(self, context, event):
 		kafka_ctx = context.get("kafka")
 		assert (kafka_ctx is not None)
 
 		key = kafka_ctx.key
-		if key is not None and key == self._key:
+		if key is not None and key in self.Keys:
 			return event
 		else:
 			return None
-
-	def process_keys(self, context, event):
-		kafka_ctx = context.get("kafka")
-		assert (kafka_ctx is not None)
-
-		key = kafka_ctx.key
-		if key is not None and key in self._keys:
-			return event
-		else:
-			return None
-
-
-	# simple version
-
-	# def __init__(self, app, pipeline, keys, id=None, config=None):
-	# 	super().__init__(app, pipeline, id, config)
-	# 	if not isinstance (keys,list):
-	# 		self._keys = [keys]
-	# 	else:
-	# 		self._keys = keys
-	#
-	#
-	#
-	# def process(self, context, event):
-	# 	kafka_ctx = context.get("kafka")
-	# 	assert (kafka_ctx is not None)
-	#
-	# 	key = kafka_ctx.key
-	# 	if key is not None and key in self._keys:
-	# 		return event
-	# 	else:
-	# 		return None
