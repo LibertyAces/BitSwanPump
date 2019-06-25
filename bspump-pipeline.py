8c14719f9784862024397eab4b1c3e14b8ca3190
Premysl Cerny <premysl.cerny@teskalabs.com>
2019-06-25 10:38:42 +0000

    Update bspump-pipeline.py

--- bspump-pipeline.py
+++ bspump-pipeline.py
@@ -34,10 +34,6 @@ if __name__ == '__main__':
 
 	svc = app.get_service("bspump.PumpService")
 
-	# Create and register all connections here
-
-	# Create and register all lookups here
-
 	# Create and register all pipelines here
 
 	pl = SamplePipeline(app, 'SamplePipeline')
