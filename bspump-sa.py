b931a4b8f427bd1a2a3933f6b63548e057d30145
margirova <margarita.argirova@teskalabs.com>
2019-06-27 17:53:32 +0200

    removed twa

--- bspump-sa.py
+++ bspump-sa.py
@@ -32,62 +32,8 @@ class MyPipeline(Pipeline):
 		self.build(
 			FileCSVSource(app, self, config={'path': "bspump/analyzer/var/users.csv", 'post':'noop'}).on(OpportunisticTrigger(app)),
 			GraphSessionAnalyzer(app, self, column_formats=['S40', 'i8'], column_names=["user_link", "duration"]), 
-			UserTimeWindowAnalyzer(app, self),
-			# PPrintSink(app, self)
 			NullSink(app, self)
 		)
-
-
-class UserTimeWindowAnalyzer(TimeWindowAnalyzer):
-	def __init__(self, app, pipeline, id=None, config=None):
-		super().__init__(app, pipeline, tw_format='S20', tw_dimensions=(2,2), resolution=2, clock_driven_analyze=True, id=id, config=config)
-
-
-	
-	def predicate(self, context, event):
-
-		if '@timestamp' not in event:
-			return False
-
-		if event["@timestamp"] == '':
-			return False
-
-		if 'user' not in event:
-			return False
-
-		return True
-
-	
-	def evaluate(self, context, event):
-		user = event["user"]
-		msg = event.get('message')
-		er = event.get('error')
-		timestamp = int(event["@timestamp"])
-		column = self.TimeWindow.get_column(timestamp)
-		if user not in self.TimeWindow.RowMap:
-			self.TimeWindow.add_row(user)
-
-		if msg is not None:
-			user_ind = self.TimeWindow.RowMap[user]
-			self.Matrix[user_ind][column, 0] = msg
-
-		if er is not None:
-			self.Matrix[self.TimeWindow.RowMap[user]][column, 1] = er
-
-	
-	async def analyze(self):
-		print("Analyzing ....")
-		tw_snapshot = self.Matrix
-		for i in range(0, tw_snapshot.shape[0]):
-			for j in range(0, 2):
-				if tw_snapshot[i][j, 1] == b'':
-					print('user {} no error'.format(self.TimeWindow.RevRowMap[i]))
-				else:
-					print('user {} error message {}'.format(self.TimeWindow.RevRowMap[i], tw_snapshot[i][j, 0]))
-
-			self.TimeWindow.close_row(self.TimeWindow.RevRowMap[i])
-		self.TimeWindow.rebuild_rows("partial")
-
 
 
 class GraphSessionAnalyzer(SessionAnalyzer):
