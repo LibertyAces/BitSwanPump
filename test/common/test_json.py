383a715da6f87639c595090957e124e76b2486e5
Jakub Boukal <www.bagr@gmail.com>
2019-07-02 10:47:13 +0200

    PLUM-125: Add tests for json processor
    http://jira.teskalabs.int:8080/browse/PLUM-125

---
+++ test/common/test_json.py
@@ -0,0 +1,54 @@
+from unittest.mock import MagicMock
+
+import bspump.unittest
+from bspump.common import DictToJsonParser
+
+
+class TestDictToJsonParser(bspump.unittest.ProcessorTestCase):
+
+	def test_dict_to_json_parser(self):
+		events = [
+			(None, {'foo': 'bar'}),
+			(None, {'fizz': 'buzz'}),
+			(None, {'spam': 'eggs'}),
+		]
+
+		self.setUpProcessor(DictToJsonParser)
+
+		output = self.execute(
+			events
+		)
+
+		self.assertEqual(
+			[event for context, event in output],
+			['{"foo": "bar"}', '{"fizz": "buzz"}', '{"spam": "eggs"}']
+		)
+
+
+class TestDictToJsonParserNotDictionary(bspump.unittest.ProcessorTestCase):
+
+	def test_event_not_dict(self):
+		# pipeline = UnitTestPipeline(app, processor)
+
+		events = [
+			(None, "Not a dictionary"),
+		]
+		# TODO self.assertTrue(pipeline.is_error())
+		# TODO self.assertIn(AssertionError(), pipeline._error)
+		# TODO mock set_error
+
+		self.setUpProcessor(DictToJsonParser)
+		svc = self.App.get_service("bspump.PumpService")
+		pipeline = svc.locate("UnitTestPipeline")
+		pipeline.set_error = MagicMock()
+		pipeline.L = MagicMock()  # turn off logging
+		# pipeline.set_error({}, 'Not a dictionary', AssertionError())
+
+		output = self.execute(events)
+		# <class 'tuple'>: ({}, 'Not a dictionary', AssertionError())
+
+		self.assertEqual(
+			[event for context, event in output],
+			[]
+		)
+
