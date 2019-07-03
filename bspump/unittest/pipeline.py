b6b7e9020c82b30de0b90a1853c9f3db5b9edda2
Ales Teska <ales.teska@teskalabs.com>
2019-07-03 11:29:26 +0200

    Extends unit test to work nicely.

--- bspump/unittest/pipeline.py
+++ bspump/unittest/pipeline.py
@@ -6,13 +6,29 @@ from ..trigger import RunOnceTrigger
 
 class UnitTestPipeline(Pipeline):
 
+
 	def __init__(self, app, processor):
 		super().__init__(app, "UnitTestPipeline")
 
 		self.Source = UnitTestSource(app, self).on(
+<<<<<<< HEAD
 			RunOnceTrigger(app)
+=======
+			bspump.trigger.PubSubTrigger(app, "unittest.go!", self.PubSub)
+>>>>>>> Extends unit test to work nicely.
 		)
 		self.Processor = processor(app, self)
 		self.Sink = UnitTestSink(app, self)
 
+		self.PubSub.subscribe("bspump.pipeline.cycle_end!", self._on_finished)
+
 		self.build(self.Source, self.Processor, self.Sink)
+
+
+	def unittest_start(self):
+		# Start the pipeline
+		self.PubSub.publish("unittest.go!")
+
+
+	def _on_finished(self, event_name, pipeline):
+		self.App.stop()
--- bspump/unittest/unit_test_case.py
+++ bspump/unittest/unit_test_case.py
@@ -1,4 +1,9 @@
 import unittest
+import logging
+
+import asab.abc.singleton
+import bspump
+
 
 from .pipeline import UnitTestPipeline
 from ..application import BSPumpApplication
@@ -10,8 +15,20 @@ class ProcessorTestCase(unittest.TestCase):
 	This is processor test case
 	'''
 
+
 	def setUp(self) -> None:
+<<<<<<< HEAD
 		self.App = BSPumpApplication()
+=======
+		self.App = bspump.BSPumpApplication(args=[])
+
+	def tearDown(self):
+		asab.abc.singleton.Singleton.delete(self.App.__class__)
+		self.App = None
+		root_logger = logging.getLogger()
+		root_logger.handlers = []
+
+>>>>>>> Extends unit test to work nicely.
 
 	def set_up_processor(self, processor):
 
@@ -20,6 +37,7 @@ class ProcessorTestCase(unittest.TestCase):
 		self.Pipeline = UnitTestPipeline(self.App, processor)
 		svc.add_pipeline(self.Pipeline)
 
+
 	def execute(self, input_data):
 		'''
 		This is universal and become part of bspump.unitest.ProcessorTestCase module
@@ -27,7 +45,7 @@ class ProcessorTestCase(unittest.TestCase):
 
 		self.Pipeline.Source.Input = input_data
 
+		self.Pipeline.unittest_start()
 		self.App.run()
 
 		return self.Pipeline.Sink.Output
-
