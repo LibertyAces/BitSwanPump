ee32966d17a5332be433c27e008698570896b499
Jakub Boukal <www.bagr@gmail.com>
2019-07-04 16:07:37 +0200

    PLUM-403: Add JsonToDictParser unit-test
    http://jira.teskalabs.int:8080/browse/PLUM-403

--- test/common/test_json.py
+++ test/common/test_json.py
@@ -39,3 +39,39 @@ class TestDictToJsonParser(bspump.unittest.ProcessorTestCase):
 			[event for context, event in output],
 			[]
 		)
+
+
+class TestJsonToDictParser(bspump.unittest.ProcessorTestCase):
+
+	def test_json_to_dict(self):
+		events = {
+			(None, '{"key": "1"}'),
+			(None, '{"key": "2"}'),
+			(None, '{"key": "3"}'),
+		}
+
+		self.set_up_processor(bspump.common.JsonToDictParser)
+
+		output = self.execute(
+			events
+		)
+
+		self.assertListEqual(
+			sorted([event for context, event in output], key=lambda d: int(d["key"])),
+			[{"key": "1"}, {"key": "2"}, {"key": "3"}]
+		)
+
+
+	def test_event_not_str(self):
+		events = [
+			(None, {"Not a": "string"}),
+		]
+		self.set_up_processor(bspump.common.JsonToDictParser)
+
+		output = self.execute(events)
+
+		self.assertTrue(self.Pipeline.is_error())
+		self.assertEqual(
+			[event for context, event in output],
+			[]
+		)
