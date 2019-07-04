644dbe3276cfea5ebc9010b0bdc526e4555c7948
Jakub Boukal <www.bagr@gmail.com>
2019-07-04 19:31:41 +0200

    PLUM-403: Add unit-test for bspump.common.jsonbytes
    http://jira.teskalabs.int:8080/browse/PLUM-403

--- test/common/__init__.py
+++ test/common/__init__.py
@@ -1,5 +1,6 @@
-from .test_json import *
 from .test_bytes import *
 from .test_flatten import *
 from .test_hexlify import *
 from .test_iterator import *
+from .test_json import *
+from .test_jsonbytes import *
