953940194f6fc44dce0b314a23c9724febba0e28
Jakub Boukal <www.bagr@gmail.com>
2019-07-17 18:30:36 +0200

    PLUM-403: Extend filter.TimeDriftFilter unit-test for pubsub.flush
    http://jira.teskalabs.int:8080/browse/PLUM-403

--- test/filter/test_timedriftfilter.py
+++ test/filter/test_timedriftfilter.py
@@ -6,11 +6,14 @@ import bspump.unittest
 
 class TestTimeDriftFilter(bspump.unittest.ProcessorTestCase):
 
+	@patch("asab.metrics.metrics.Counter.flush")
 	@patch("asab.application.Application.time")
-	def test_time_drift_filter(self, mocked_time):
+	def test_time_drift_filter(self, mocked_time, mocked_flush):
 		mocked_time.return_value = 100
 		events = [
 			(None, {"@timestamp": 40}),
+			(None, {"@timestamp": 39}),
+			(None, {"no": "timestamp"}),
 		]
 
 		self.set_up_processor(bspump.filter.TimeDriftFilter)
@@ -23,3 +26,8 @@ class TestTimeDriftFilter(bspump.unittest.ProcessorTestCase):
 			[event for context, event in output],
 			[{'@timestamp': 40}]
 		)
+		self.assertEqual(
+			self.Pipeline.Processor.TimeDriftFilterCounter.rest_get()["Values"],
+			{'event.in': 3, 'event.out': 1, 'event.drop': 1, 'timestamp.error': 1}
+		)
+		self.assertEqual(mocked_flush.call_count, 2)
