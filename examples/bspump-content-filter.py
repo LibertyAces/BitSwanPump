5de2e9f853e219a5006fb8ec65a46232e6689e47
Jakub Boukal <www.bagr@gmail.com>
2019-07-31 18:23:20 +0200

    PLUM-432: Add users.csv for - and fix - content-filter example
    http://jira.teskalabs.int:8080/browse/PLUM-432

--- examples/bspump-content-filter.py
+++ examples/bspump-content-filter.py
@@ -1,20 +1,21 @@
-from bspump import BSPumpApplication, Pipeline, Processor
-from bspump.file import FileCSVSource
-from bspump.trigger import OpportunisticTrigger
-from bspump.common import PPrintSink, NullSink
-from bspump.filter import ContentFilter
-
 import logging
-import string
 import random
+import string
 
+import bspump
+import bspump.common
+import bspump.file
+import bspump.filter
+import bspump.trigger
 
 ##
+
 L = logging.getLogger(__name__)
+
 ##
 
 
-class MyApplication(BSPumpApplication):
+class MyApplication(bspump.BSPumpApplication):
 	def __init__(self):
 		super().__init__()
 
@@ -22,44 +23,28 @@ class MyApplication(BSPumpApplication):
 		svc.add_pipeline(MyPipeline(self))
 		
 
-class MyPipeline(Pipeline):
+class MyPipeline(bspump.Pipeline):
 	def __init__(self, app, pipeline_id=None):
 		super().__init__(app, pipeline_id)
-		query = {
-			"$or":
-				[
-					{
-						"user":
-							{
-								'$in':["user_0", "user_2", "user_10"]
-							}
-					}, 
-					{
-						"num":
-							{
-								"$gte": 500
-							}
-					},
-					{
-						"st":{
-							"$regex":"/^L/i"
-						}
-					}
-				]
-		}
+
+		query = {'$or': [
+			{'user': {'$in': ['user_0', 'user_2', 'user_10']}},
+			{'num': {'$gte': 500}},
+			{'st': {'$regex': '/^L/i'}}
+		]}
 		self.build(
-			FileCSVSource(app, self, 
-				config={
-					"post":"noop",
-					"path":"test.csv"  # TODO add missing data file
-				}).on(OpportunisticTrigger(app)),
-			RandomProcessor(app, self), 
+			bspump.file.FileCSVSource(app, self, config={
+				"post": "noop",
+				"path": "./data/users.csv"
+			}).on(bspump.trigger.OpportunisticTrigger(app)),
+
+			RandomProcessor(app, self),
 			MyContentFilter(app, self, query),
-			PPrintSink(app, self),
+			bspump.common.PPrintSink(app, self),
 		)
 
 
-class RandomProcessor(Processor):
+class RandomProcessor(bspump.Processor):
 	
 	def process(self, context, event):
 		st = ''.join(random.choice(string.ascii_uppercase) for _ in range(4))
@@ -70,7 +55,7 @@ class RandomProcessor(Processor):
 		return event
 
 
-class MyContentFilter(ContentFilter):
+class MyContentFilter(bspump.filter.ContentFilter):
 	def on_hit(self, context, event):
 		event["tag"] = ":)"
 		return event
@@ -79,7 +64,6 @@ class MyContentFilter(ContentFilter):
 	def on_miss(self, context, event):
 		event["tag"] = ":("
 		return event
-
 
 
 if __name__ == '__main__':
