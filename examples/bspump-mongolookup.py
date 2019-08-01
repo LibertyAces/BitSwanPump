019ca02ff5d2084784234373fae4b24b9b647eee
Jakub Boukal <www.bagr@gmail.com>
2019-08-01 18:27:28 +0200

    PLUM-432: Fix imports on mongolookup example
    http://jira.teskalabs.int:8080/browse/PLUM-432

--- examples/bspump-mongolookup.py
+++ examples/bspump-mongolookup.py
@@ -1,60 +1,64 @@
-from bspump.mongodb import MongoDBLookup, MongoDBConnection
-
-from bspump.file import FileCSVSource
-from bspump.trigger import OpportunisticTrigger
-from bspump.common import PPrintSink, NullSink
-from bspump import BSPumpApplication, Pipeline, Processor
 import logging
+
+import bspump
 import bspump.common
+import bspump.file
+import bspump.mongodb
+import bspump.trigger
 
 ##
+
 L = logging.getLogger(__name__)
+
+
 ##
 
-class MyApplication(BSPumpApplication):
+
+class MyApplication(bspump.BSPumpApplication):
 	def __init__(self):
 		super().__init__()
 
 		svc = self.get_service("bspump.PumpService")
 
-		mongodb_connection = MongoDBConnection(self, "MongoDBConnection", config={
-			"host":"mongodb://127.0.0.1:27017"})
-		
-		self.MongoDBLookup = MongoDBLookup(self,
-			connection=mongodb_connection,
-			id="MongoDBLookup",
-			config={
-				'collection':'user_location',
-				'database': 'users',
-				'key': 'user'
-			})
+		mongodb_connection = bspump.mongodb.MongoDBConnection(self, "MongoDBConnection", config={
+			"host": "mongodb://127.0.0.1:27017",
+		})
+
+		self.MongoDBLookup = bspump.mongodb.MongoDBLookup(self, mongodb_connection, "MongoDBLookup", config={
+			'collection': 'user_location',
+			'database': 'users',
+			'key': 'user',
+		})
 
 		svc.add_lookup(self.MongoDBLookup)
 		svc.add_pipeline(MyPipeline(self))
-		
 
-class MyPipeline(Pipeline):
+
+class MyPipeline(bspump.Pipeline):
 	# Enriches the event with location from Mogodb lookup
 	def __init__(self, app, pipeline_id=None):
 		super().__init__(app, pipeline_id)
 		self.build(
-			FileCSVSource(app, self, 
-				config={"post":"noop", "path":"bspump/mongodb/var/users.csv"}
-				).on(OpportunisticTrigger(app)),
-			MyProcessor(app, self), 
-			PPrintSink(app, self)
-			# NullSink(app, self)
+
+			bspump.file.FileCSVSource(app, self, config={
+				"post": "noop",
+				"path": "./data/users.csv"
+			}).on(bspump.trigger.OpportunisticTrigger(app)),
+
+			MyProcessor(app, self),
+			bspump.common.PPrintSink(app, self),
+			# bspump.common.NullSink(app, self),
 
 		)
 
 
-class MyProcessor(Processor):
+class MyProcessor(bspump.Processor):
 
 	def __init__(self, app, pipeline, id=None, config=None):
 		super().__init__(app, pipeline, id, config)
 		svc = app.get_service("bspump.PumpService")
 		self.Lookup = svc.locate_lookup("MongoDBLookup")
-	
+
 	def process(self, context, event):
 		if 'user' not in event:
 			return None
@@ -62,10 +66,8 @@ class MyProcessor(Processor):
 		info = self.Lookup.get(event['user'])
 		if info is not None:
 			event['L'] = info.get('L')
-		
+
 		return event
-
-
 
 
 if __name__ == '__main__':
