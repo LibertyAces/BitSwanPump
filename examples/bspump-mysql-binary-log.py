55070f441cbcadd3d3a24bf44f3ed0ee9df6b64e
Jakub Boukal <www.bagr@gmail.com>
2019-08-05 15:04:11 +0200

    PLUM-432: Add description to mysql-binary-log example
    http://jira.teskalabs.int:8080/browse/PLUM-432

--- examples/bspump-mysql-binary-log.py
+++ examples/bspump-mysql-binary-log.py
@@ -14,15 +14,48 @@ L = logging.getLogger(__name__)
 
 
 class MyApplication(bspump.BSPumpApplication):
+	"""
+
+	## Try it out
+
+	# Create and chown mysql lock
+
+	$ sudo mkdir /var/run/mysqld
+	$ sudo chown 999:999 /var/run/mysqld
+
+	$ docker run --rm \
+		-v /tmp/mysql_datadir:/var/lib/mysql \
+		-v /var/run/mysqld/:/var/run/mysqld/ \
+		-p 3306:3306 \
+		-e MYSQL_ROOT_PASSWORD=root_password \
+		mysql \
+		mysqld \
+		--datadir=/var/lib/mysql \
+		--server-id=1 \
+		--log-bin=/var/lib/mysql/mysql-bin.log \
+		--binlog_do_db=sample_db
+
+	# Insert some sample data in your database
+	```
+		mysql> create database sample_db;
+		mysql> use sample_db;
+		mysql> CREATE TABLE people (id INT NOT NULL AUTO_INCREMENT, name CHAR(30), surname CHAR(30), PRIMARY KEY (id));
+		mysql> INSERT INTO people (name, surname) VALUES ("john", "doe"),("juan", "perez"),("wop", "wops");
+	```
+	# Chown binlog
+
+	$ sudo chmod a+r /tmp/mysql_datadir/mysql-bin.000001
+
+	"""
 	def __init__(self):
 		super().__init__()
 
 		svc = self.get_service("bspump.PumpService")
 
 		mysql_connection = bspump.mysql.MySQLConnection(self, "MySQLConnection", config={
-			"user": "user",
-			"password": "password",
-			"db": "users"
+			"user": "root",
+			"password": "root_password",
+			"db": "sample_db"
 		})
 
 		svc.add_connection(mysql_connection)
@@ -35,7 +68,7 @@ class MyPipeline(bspump.Pipeline):
 		self.build(
 			bspump.mysql.binlogsource.MySQLBinaryLogSource(app, self, "MySQLConnection", config={
 				'server_id': 1,
-				'log_file': 'mysql-bin.000001'
+				'log_file': '/tmp/mysql_datadir/mysql-bin.000001'
 			}),
 			bspump.common.PPrintSink(app, self)
 		)
