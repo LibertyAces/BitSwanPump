5a3efc9f6fa02be7295f1f6ead1289419029cea2
Jakub Boukal <www.bagr@gmail.com>
2019-08-05 12:00:29 +0200

    PLUM-432: Optimize imports on mysql-binary-log example
    http://jira.teskalabs.int:8080/browse/PLUM-432

--- examples/bspump-mysql-binary-log.py
+++ examples/bspump-mysql-binary-log.py
@@ -1,41 +1,44 @@
-from bspump.mysql import MySQLConnection
-from bspump.mysql.binlogsource import MySQLBinaryLogSource
-from bspump.trigger import OpportunisticTrigger
-from bspump.common import PPrintSink
-from bspump import BSPumpApplication, Pipeline, Processor
 import logging
+
+import bspump
 import bspump.common
+import bspump.mysql
+import bspump.mysql.binlogsource
 
 ##
+
 L = logging.getLogger(__name__)
+
+
 ##
 
-class MyApplication(BSPumpApplication):
+
+class MyApplication(bspump.BSPumpApplication):
 	def __init__(self):
 		super().__init__()
 
 		svc = self.get_service("bspump.PumpService")
 
-		mysql_connection = MySQLConnection(self, "MySQLConnection", config={
-			"user":"user",
-			"password":"password",
-			"db":"users"
+		mysql_connection = bspump.mysql.MySQLConnection(self, "MySQLConnection", config={
+			"user": "user",
+			"password": "password",
+			"db": "users"
 		})
 
 		svc.add_connection(mysql_connection)
 		svc.add_pipeline(MyPipeline(self))
-		
 
-class MyPipeline(Pipeline):
+
+class MyPipeline(bspump.Pipeline):
 	def __init__(self, app, pipeline_id=None):
 		super().__init__(app, pipeline_id)
 		self.build(
-			MySQLBinaryLogSource(app, self, "MySQLConnection",
-				config={'server_id': 1, 'log_file': 'mysql-bin.000001'}
-			),
-			PPrintSink(app, self)
+			bspump.mysql.binlogsource.MySQLBinaryLogSource(app, self, "MySQLConnection", config={
+				'server_id': 1,
+				'log_file': 'mysql-bin.000001'
+			}),
+			bspump.common.PPrintSink(app, self)
 		)
-
 
 
 if __name__ == '__main__':
