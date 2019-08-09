a30144baaf7e06763da42a7b10b6f8fcc465ae07
margirova <margarita.argirova@teskalabs.com>
2019-08-09 21:27:00 +0200

    database listening enabled

--- bspump/mongodb/changestreamsource.py
+++ bspump/mongodb/changestreamsource.py
@@ -1,7 +1,7 @@
 import logging
 from ..abc.source import Source
 import asyncio
-
+import pymongo
 #
 
 L = logging.getLogger(__name__)
@@ -9,6 +9,11 @@ L = logging.getLogger(__name__)
 #
 
 class MongoDBChangeStreamSource(Source):
+	'''
+		Make sure, that version of MongoDB is >= 4.0.0 and
+		replica set is enabled.
+		TODO: add queries to match. 
+	'''
 
 	ConfigDefaults = {
 		'database':'',
@@ -21,14 +26,19 @@ class MongoDBChangeStreamSource(Source):
 		self.Connection = pipeline.locate_connection(app, connection)
 		self.Database = self.Config['database']
 		self.Collection = self.Config['collection']
+		if self.Collection == '':
+			self.Collection = None
 	
 
 	async def main(self):
 		running = True
-		db = self.Connection.Client[self.Database]
-
 		await self.Pipeline.ready()
-		stream = db[self.Collection].watch()
+		db = self.Connection.Client[self.Database]
+		if self.Collection is None:
+			stream = db.watch()
+		else:
+			stream = db[self.Collection].watch()
+		
 		while True:
 			if not running:
 				await stream.close()
