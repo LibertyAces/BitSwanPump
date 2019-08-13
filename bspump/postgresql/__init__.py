3b28e620373ee8a3ca67c33e3bcdb4b262e91500
margirova <margarita.argirova@teskalabs.com>
2019-08-13 11:18:28 +0200

    raw source

--- bspump/postgresql/__init__.py
+++ bspump/postgresql/__init__.py
@@ -1,3 +1,5 @@
 from .connection import PostgreSQLConnection
 from .sink import PostgreSQLSink
+from .walsource import PostgreSQLWriteAheadLogSource
+
 
---
+++ examples/bspump-postgres-wal-source.py
@@ -0,0 +1,33 @@
+from bspump import BSPumpApplication, Pipeline
+import bspump.trigger
+import bspump.common
+import bspump.postgresql
+import logging
+
+
+
+##
+L = logging.getLogger(__name__)
+##
+
+
+class MyApplication(BSPumpApplication):
+	def __init__(self):
+		super().__init__()
+		svc = self.get_service("bspump.PumpService")
+		svc.add_connection(bspump.mongodb.PostgreSQLConnection(self, config={})) #TODO!
+		svc.add_pipeline(MyPipeline0(self))
+		
+
+class MyPipeline0(Pipeline):
+	def __init__(self, app, pipeline_id=None):
+		super().__init__(app, pipeline_id)
+		self.build(
+			bspump.mongodb.PostgreSQLWriteAheadLogSource(app, self, "PostgreSQLConnection"),
+			bspump.common.PPrintSink(app, self)
+		)
+
+
+if __name__ == '__main__':
+	app = MyApplication()
+	app.run()
