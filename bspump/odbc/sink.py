b40f1734815a7d649e16f0c2c76298796ad9b7d7
jsafranek <safranek.h@gmail.com>
2019-09-04 14:44:34 +0200

    Finished ODBC Sink

--- bspump/odbc/sink.py
+++ bspump/odbc/sink.py
@@ -5,4 +5,40 @@ import asab
 from ..abc.sink import Sink
 
 class ODBCSink(Sink):
-	pass
+
+
+	ConfigDefaults = {
+		'query': '',
+		'data': '',
+	}
+
+
+	def __init__(self, app, pipeline, connection, id=None, config=None):
+		super().__init__(app, pipeline, id=id, config=config)
+
+		self._connection = pipeline.locate_connection(app, connection)
+		self._query = self.Config['query']
+		self._data_keys = self.Config['data'].split(',')
+
+		app.PubSub.subscribe("ODBConnection.pause!", self._connection_throttle)
+		app.PubSub.subscribe("ODBConnection.unpause!", self._connection_throttle)
+
+
+	def process(self, context, event):
+		# Prepare data
+		data = tuple(event.get(x) for x in self._data_keys)
+
+		# Consume
+		self._connection.consume(self._query, data)
+
+
+	def _connection_throttle(self, event_name, connection):
+		if connection != self._connection:
+			return
+
+		if event_name == "ODBConnection.pause!":
+			self.Pipeline.throttle(self, True)
+		elif event_name == "ODBCConnection.unpause!":
+			self.Pipeline.throttle(self, False)
+		else:
+			raise RuntimeError("Unexpected event name '{}'".format(event_name))
