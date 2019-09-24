3f4b6ba9c8db36b5901f3267c0987f310d13b60b
Jakub Boukal <www.bagr@gmail.com>
2019-09-24 10:37:02 +0200

    PLUM-576: Remove profiler Module
    http://jira.teskalabs.int:8080/browse/PLUM-576

--- bspump/profiler/__init__.py
+++ bspump/profiler/__init__.py
@@ -1,9 +1,2 @@
-import asab
 from .service import ProfilerService
-
-
-class Module(asab.Module):
-
-	def __init__(self, app):
-		super().__init__(app)
-		self.service = ProfilerService(app, "bspump.ProfilerService")
+from .profiler import ProfilerCounter
