9c9b9d534aa6313ec1fb342c2e06c57b765a78fd
margirova <margarita.argirova@teskalabs.com>
2019-10-02 10:23:43 +0200

    another forgotten file

---
+++ examples/matrix-lookup/bspump-matrix-lookup-master.py
@@ -0,0 +1,32 @@
+import logging
+import numpy as np
+import pandas
+
+import bspump
+import bspump.common
+import bspump.elasticsearch
+import bspump.file
+import bspump.trigger
+from lookup import MyMatrixLookup
+
+##
+
+L = logging.getLogger(__name__)
+
+##
+
+
+class MasterApplication(bspump.BSPumpApplication):
+	def __init__(self):
+		super().__init__(web_listen="0.0.0.0:8080")
+
+		svc = self.get_service("bspump.PumpService")
+		
+		self.Lookup = MyMatrixLookup(self, id='MyLookup', config={'path':'cz.o2tv.db.as.cac.live.epg.results.csv', 'delimiter':';'})
+
+		svc.add_lookup(self.Lookup)
+
+
+if __name__ == '__main__':
+	app = MasterApplication()
+	app.run()
