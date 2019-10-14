f150f4bd656eb50046271b56514edca211d814ea
F.P <pesekf@email.cz>
2019-10-14 18:31:58 +0200

    PLUM-702: Remove odd code from file
    http://jira.teskalabs.int:8080/browse/PLUM-702

--- bspump/ssh/sink.py
+++ bspump/ssh/sink.py
@@ -135,29 +135,3 @@ class SFTPSink(Sink):
 		random_num = str(random.randint(1, self.RandIntLen))
 		filename = str(timestamp+hostname+random_num)
 		return filename
-
-
-
-
-
-
-
-
-from ..abc.sink  import Sink
-
-...
-
-# import asyncio, asyncssh, sys
-#
-# async def run_client():
-#     async with asyncssh.connect('localhost') as conn:
-#         async with conn.create_process('bc') as process:
-#             for op in ['2+2', '1*2*3*4', '2^32']:
-#                 process.stdin.write(op + '\n')
-#                 result = await process.stdout.readline()
-#                 print(op, '=', result, end='')
-#
-# try:
-#     asyncio.get_event_loop().run_until_complete(run_client())
-# except (OSError, asyncssh.Error) as exc:
-#     sys.exit('SSH connection failed: ' + str(exc))
