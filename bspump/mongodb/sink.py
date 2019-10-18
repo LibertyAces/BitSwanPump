b909dc6e2102461cd363f0b9c6fc5229f0279dde
Jakub Schier <jakub.schier@mojemana.cz>
2019-10-18 11:50:15 +0200

    Add while loop with break condition and process throttling when queue overflows

--- bspump/mongodb/sink.py
+++ bspump/mongodb/sink.py
@@ -16,18 +16,19 @@ class MongoSink(Sink):
         super().__init__(app, pipeline, id, config)
 
         self.Connection = pipeline.locate_connection(app, connection)
+        self.Pipeline = pipeline
         self._output_queue = asyncio.Queue()
         self._output_queue_max_size = int(self.Config['output_queue_max_size'])
         assert (self._output_queue_max_size >= 1), "Output queue max size invalid"
         self._conn_future = None
 
-        self._on_health_check()
+        self._on_health_check("Connection.open!")
 
         app.PubSub.subscribe("Application.stop!", self._on_application_stop)
         app.PubSub.subscribe("Application.tick!", self._on_health_check)
         app.PubSub.subscribe("Application.exit!", self._on_exit)
 
-    def _on_health_check(self):
+    def _on_health_check(self, message_type):
         if self._conn_future is not None:
 
             if not self._conn_future.done():
@@ -52,12 +53,19 @@ class MongoSink(Sink):
             await asyncio.wait([self._conn_future], return_when=asyncio.ALL_COMPLETED, loop=self.Loop)
 
     def process(self, context, event: [dict, list]):
+        if self._output_queue.qsize() >= self._output_queue_max_size:
+            self.Pipeline.throttle(self, True)
+
         self._output_queue.put_nowait(event)
 
     async def insert_item(self):
 
         db = self.Connection.Client["test_test"]
 
-        what_for = await self._output_queue.get()
-        await db.test_collection.insert_one(what_for)
-        self._output_queue.task_done()
+        while True:
+
+            what_for = await self._output_queue.get()
+            if what_for is None:
+                break
+            await db.test_collection.insert_one(what_for)
+            self._output_queue.task_done()
