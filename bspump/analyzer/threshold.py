1d22d604360057d4573ff3ef8b273988050b17b2
F.P <pesekf@email.cz>
2019-10-23 17:28:50 +0200

    PLUM-726: WIP Sets bspump threshold analyzer example, refactors evaluate, analyze and alarm methods
    http://jira.teskalabs.int:8080/browse/PLUM-726

--- bspump/analyzer/threshold.py
+++ bspump/analyzer/threshold.py
@@ -1,10 +1,9 @@
-import time
+import datetime
 import numpy as np
 
 import logging
 
 from .timewindowanalyzer import TimeWindowAnalyzer
-from .timewindowmatrix import TimeWindowMatrix
 
 ###
 
@@ -12,20 +11,26 @@ L = logging.getLogger(__name__)
 
 ###
 
-ConfigDefaults = {
-	#TODO 'resolution': 60,  # Resolution (aka column width) in seconds
-	'event_name': '', # User defined value, e.g. server name
-	'load': '', # User defined load to matrix, if not specified (left empty), load will be the count of occurences (histogram)
-	'lower_bound': 0, # if lower bound > upper bound: alarm is set when value is below lower bound, if lower bound != 0 and upper bound > lower_bound: alarm is set when value is out of bounds
-	'upper_bound': 1000,
-}
 
 class ThresholdAnalyzer(TimeWindowAnalyzer):
-	def __init__(self, app, pipeline, id=None, config=None):
-		super().__init__(app, pipeline, matrix_id=None, dtype='float_', columns=15, analyze_on_clock=False,
-						 resolution=60, start_time=None, clock_driven=False, id=id, config=config)
 
-		# self._resolution = int(self.Config['resolution'])
+	ConfigDefaults = {
+
+		'event_name': '',  # User defined value, e.g. server name
+		'load': '', # User defined load to matrix
+		# if lower bound > upper bound: alarm is set when value is below lower bound,
+		# if lower bound != 0 and upper bound > lower_bound: alarm is set when value is out of bounds
+		'lower_bound': 0,
+		'upper_bound': 1000,
+		'analyze_period':300,
+
+	}
+
+	def __init__(self, app, pipeline, id=None, config=None):
+		super().__init__(app, pipeline, matrix_id=None, dtype='float_', columns=15, analyze_on_clock=True,
+						 resolution=60, start_time=None, clock_driven=True, id=id, config=config)
+
+
 		self.Event_name = self.Config['event_name']
 		self.Load = self.Config['load']
 		self.Lower = self.Config['lower_bound']
@@ -46,81 +51,43 @@ class ThresholdAnalyzer(TimeWindowAnalyzer):
 
 
 	def evaluate(self, context, event):
-		value = str(event[self.Event_name])  # server name e.g.
-		time_stamp = event["@timestamp"] # time stamp of the event
+		value = event[self.Event_name]
+		time_stamp = event["@timestamp"]
 
 		row = self.TimeWindow.get_row_index(value)
 		if row is None:
 			row = self.TimeWindow.add_row(value)
 
-		# find the column in timewindow matrix to fit in
+		# Find the column in timewindow matrix to fit in
 		column = self.TimeWindow.get_column(time_stamp)
 		if column is None:
-			return
+			column = self.TimeWindow.get_column(time_stamp)
 
-		# load
-		if self.Load == '':
-			self.TimeWindow.Array[row, column] += 1
-		else:
-			self.TimeWindow.Array[row, column] = event[self.Load]
+		# Load
+		self.TimeWindow.Array[row, column] = event[self.Load]
 
 
 	def analyze(self): #TODO set analyzing method
-		print(self.TimeWindow.Array.shape[0])
-		if self.TimeWindow.Array.shape[0] == 0: # checking an empty array
+		# checking an empty array
+		if self.TimeWindow.Array.shape[0] == 0:
 			return
 
-		#TODO Check if this below is not a better solution
-		# use np function to analyze threshold
-		# 	...
+		data = self.TimeWindow.Array[:, :]
+		# Changing zero values to np.nan
+		data[data == 0] = np.nan
+		median_matrix_value = np.nanmedian(data[:, :])
 
-		# if not self.Lower <= 0 and self.Lower < self.Upper: # range
-		# 	if self.TimeWindow.Array.shape[0] < self.Lower or self.TimeWindow.Array.shape[0] > self.Upper:
-		# 		self.alarm()  # call alarm method
-		# elif self.Lower > self.Upper: # below the limit
-		# 	if self.TimeWindow.Array.shape[0] < self.Lower:
-		# 		self.alarm()  # call alarm method
-		# elif self.Lower == 0 and self.Upper > self.Lower: # above the limit
-		# 	if self.TimeWindow.Array.shape[0] > self.Upper:
-		# 		self.alarm() # call alarm method
-		# else:
-		# 	raise ValueError
+		if self.Lower == 0 and median_matrix_value > self.Upper: # exceedance
+			print(self.alarm(median_matrix_value))
+		elif self.Lower != 0 and self.Lower > self.Upper and median_matrix_value < self.Lower: # subceedance
+			print(self.alarm(median_matrix_value))
+		elif self.Lower != 0 and self.Lower < self.Upper and (median_matrix_value < self.Lower or
+															  median_matrix_value > self.Upper): # range (out of bounds)
+			print(self.alarm(median_matrix_value))
 
-		#TODO
-		# if value is in AlarmDict - hold/dont start the alarm, else: start the alarm
-		# threshold_limiter = len(line v matrixu (row))f
-		# as a len_threshold analyzer use np. function?
 
-	def alarm(self):
-		self.alarm_val = str('Threshold has been subceeded / exceeded at {} %'.format(str(datetime.datetime.now()
-																						  )[:-7].replace(" ", "T")))
-		return self.alarm_val
-
-	# def alarm(self, len_array, lower_bound, upper_bound): #TODO set alarm
-	# 	lower_bound = lower_bound
-	# 	upper_bound = upper_bound
-	#
-	# 	if not int(len_array):
-	# 		raise ValueError
-	# 	#
-	# 	# if not int(len_threshold):
-	# 	# 	raise ValueError
-	#
-	# 	if level == 'above':
-	# 		self.alarm_val = str('Threshold has been exceeded by {} %'.format(
-	# 			(abs(len_array-len_threshold) / len_threshold) * 100))
-	# 	elif level == 'below':
-	# 		self.alarm_val = str('Threshold has been subceeded by {} %'.format(
-	# 			(abs(len_threshold - len_array) / len_threshold_limiter) * 100))
-	# 	elif level == 'range':
-	# 		if len_array > len_threshold[1]:
-	# 			self.alarm_val = str('Range has been exceeded by {} %'.format(
-	# 				(abs(len_array - len_threshold[1]) / len_threshold[1]) * 100))
-	# 		elif len_threshold_limiter < len_threshold[0]:
-	# 			self.alarm_val = str('Threshold has been subceeded by {} %'.format(
-	# 				(abs(len_threshold[0] - len_threshold_limiter) / len_array) * 100))
-	# 	else:
-	# 		raise TypeError
-	#
-	# 	return self.alarm_val
-
+	def alarm(self, value):
+		alarm_result = str('Threshold has been subceeded / exceeded at {}'
+						   .format(str(datetime.datetime.now())[:-7].replace(" ", "T"))
+						   +' by value {}'.format(str(value)))
+		return alarm_result
--- examples/bspump-threshold-analyzer.py
+++ examples/bspump-threshold-analyzer.py
@@ -6,48 +6,110 @@ import bspump.amqp
 import bspump.random
 import bspump.trigger
 import bspump.analyzer
+import numpy as np
 
 import datetime
 
 
 
-class CustomAPMPipeline(bspump.Pipeline):
+class CustomPipeline(bspump.Pipeline):
     def __init__(self, app, pipeline_id):
         super().__init__(app, pipeline_id)
         self.build(
             # bspump.amqp.AMQPSource(app, self, "AMQPConnection"),
             # bspump.common.BytesToStringParser(app,self),
             bspump.random.RandomSource(app, self,
-                                       config={'number': 1000, 'upper_bound': 5, 'field': 'server'}
+                                       config={'number': 25, 'upper_bound': 5, 'field': 'server'}
                                        ).on(bspump.trigger.OpportunisticTrigger(app, chilldown_period=.1)),
             bspump.random.RandomEnricher(app, self, config={'field': 'duration', 'lower_bound': 1, 'upper_bound': 1500},
                                          id="RE0"),
             bspump.random.RandomEnricher(app, self, config={'field': '@timestamp',
                                                             'lower_bound': int(datetime.datetime.timestamp(datetime.datetime.now())-5),
                                                             'upper_bound': int(datetime.datetime.timestamp(datetime.datetime.now()))}, id="RE1"),
-            MyThresholdAnalyzer(app,self,config={'event_name':'server', 'lower_bound':0,'upper_bound':1000, 'load':'duration',}),
+            MyThresholdAnalyzer(app,self,config={'event_name':'server', 'lower_bound':50,'upper_bound':300, 'load':'duration',}), # 'load':'duration'
             # bspump.common.PPrintSink(app, self),
             bspump.common.NullSink(app, self),
         )
 
 
 class MyThresholdAnalyzer(bspump.analyzer.ThresholdAnalyzer):#TODO configure test analyzer
+
+    ConfigDefaults = {
+
+        'event_name': '',  # User defined value, e.g. server name
+        'load': '',
+        # if lower bound > upper bound: alarm is set when value is below lower bound, if lower bound != 0 and upper bound > lower_bound: alarm is set when value is out of bounds
+        'lower_bound': 0,
+        'upper_bound': 1000,
+        'analyze_period': 5, # Will start analyze method over defined period (in seconds)
+
+    }
+
     def __init__(self, app, pipeline, id=None, config=None):
-        super().__init__(app, pipeline, id=id, config=config)
+        super().__init__(app, pipeline,id=id, config=config)
 
-        # self.Split =
-        # self._event_name =
+        self.Event_name = self.Config['event_name']
+        self.Load = self.Config['load']
+        self.Lower = self.Config['lower_bound']
+        self.Upper = self.Config['upper_bound']
+        self.Row = []
 
-    #TODO
-    # def predicate(self, context, event):
-        # var = str(event).split(self.Split)
-        # for attr in var:
-        # 	if self._event_name in attr:
-        # 		print(attr)
-        # 	else:
-        # 		print(attr, type(attr), 'timestaaaaaamp')
-        # if "@timestamp" in attr:
-        # 	print(attr, 'timestaaaaaaamp')
+        self.TimeWindow.zeros()  # initializing timewindow with zeros
+
+    # check if event contains related fields
+    def predicate(self, context, event):
+        if self.Event_name not in event:
+            return False
+
+        if "@timestamp" not in event:
+            return False
+
+        return True
+
+    def evaluate(self, context, event):
+        value = event[self.Event_name]  # server name e.g.
+        time_stamp = event["@timestamp"]  # time stamp of the event
+
+        row = self.TimeWindow.get_row_index(value)
+
+        if row is None:
+            row = self.TimeWindow.add_row(value)
+            if row not in self.Row:
+                self.Row += [row]
+
+        # find the column in timewindow matrix to fit in
+        column = self.TimeWindow.get_column(time_stamp)
+        if column is None:
+            column = self.TimeWindow.get_column(time_stamp)
+
+        # Load, creates histogram of occurences
+        self.TimeWindow.Array[row, column] +=1
+
+    def analyze(self):
+        # checking an empty array
+        if self.TimeWindow.Array.shape[0] == np.nan:
+            return
+
+        data = self.TimeWindow.Array[:, :]
+        # Changing zero values to np.nan
+        data[data == 0] = np.nan
+        for i in self.Row:
+            median_matrix_value = np.nanmedian(data[i, :])
+            if self.Lower == 0 and median_matrix_value > self.Upper:  # exceedance
+                print(self.alarm(median_matrix_value, i))
+            elif self.Lower != 0 and self.Lower > self.Upper and median_matrix_value < self.Lower:  # subceedance
+                print(self.alarm(median_matrix_value, i))
+            elif self.Lower != 0 and self.Lower < self.Upper and (median_matrix_value < self.Lower or
+                                                                  median_matrix_value > self.Upper):  # range (out of bounds)
+                print(self.alarm(median_matrix_value, i))
+
+
+    def alarm(self, value, id):
+        alarm_result = str('Threshold has been subceeded / exceeded at {}'.format(str(datetime.datetime.now()
+                                                                                      )[:-7].replace(" ", "T"))
+                           + ' by value {}'.format(str(value))
+                           + ' of server id: {}'.format(str(id)))
+        return alarm_result
 
 
 if __name__=='__main__':
@@ -62,7 +124,7 @@ if __name__=='__main__':
 
     # Constr & register pipeline
     svc.add_pipeline(
-        CustomAPMPipeline(app, "CustomAPMPipeline"),
+        CustomPipeline(app, "CustomPipeline"),
     )
 
     app.run()
