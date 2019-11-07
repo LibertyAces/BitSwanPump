2ed84381940170fa10cdb1a1041dd03ce9c31555
Jakub Schier <jakub.schier@mojemana.cz>
2019-11-07 14:34:35 +0100

    Small refactoring

--- bspump/file/filecsvsink.py
+++ bspump/file/filecsvsink.py
@@ -4,14 +4,15 @@ from ..abc.sink import Sink
 import asyncio
 from datetime import datetime
 from asab.timer import Timer
+
 #
 
 L = logging.getLogger(__file__)
 
+
 #
 
 class FileCSVSink(Sink):
-
 	ConfigDefaults = {
 		'path': '',
 		'dialect': 'excel',
@@ -34,9 +35,9 @@ class FileCSVSink(Sink):
 		assert (self._output_queue_max_size >= 1), "Output queue max size invalid"
 		self._conn_future = None
 		self._output_queue = asyncio.Queue()
-#		app.PubSub.subscribe("Application.tick!", self.write_to_file)
+		#		app.PubSub.subscribe("Application.tick!", self.write_to_file)
 		self.num = 0
-		self.FlushPeriod = int(10)
+		self.FlushPeriod = int(4)
 		self.AnalyzeTimer = Timer(app, self.write_to_file, autorestart=True)
 		self.AnalyzeTimer.start(self.FlushPeriod)
 
@@ -45,7 +46,6 @@ class FileCSVSink(Sink):
 		Override this method to gain control over output file name.
 		'''
 		return self.Config['path']
-
 
 	def writer(self, f, fieldnames):
 		kwargs = dict()
@@ -64,25 +64,19 @@ class FileCSVSink(Sink):
 		kwargs['strict'] = bool(self.Config.get('strict'))
 
 		return csv.DictWriter(f,
-			dialect=self.Dialect,
-			fieldnames=fieldnames,
-			**kwargs
-		)
+		                      dialect=self.Dialect,
+		                      fieldnames=fieldnames,
+		                      **kwargs
+		                      )
 
 	async def write_to_file(self):
+
 		while True:
-			self.num +=1
 			context, item = await self._output_queue.get()
-			print(context)
-			print(item)
-			if self._output_queue.qsize() == 0:
-				print("Queue empty")
-				break
+
 			if self._csv_writer is None:
 				# Open CSV file if needed
 				fieldnames = item.keys()
-				print(type(item))
-				print(context)
 				fname = str(datetime.now().time()).replace(":", "_").replace(".", "_") + ".csv"
 				fo = open(fname, 'w', newline='')
 				self._csv_writer = self.writer(fo, fieldnames)
@@ -90,17 +84,16 @@ class FileCSVSink(Sink):
 				print("saving..")
 			self._csv_writer.writerow(item)
 			self._output_queue.task_done()
-			print(f"druha{self._output_queue.qsize()}")
-		self.rotate()
+
+			if self._output_queue.qsize() == 0:
+				self.rotate()
+				break
 
 	def process(self, context, event: [dict, list]):
-		# This is where we check if the queue is overflowing in which case we apply throttling.
 		if self._output_queue.qsize() >= self._output_queue_max_size:
 			self.Pipeline.throttle(self, True)
-		# Here,  we take the event (in this case it should be either a dictionary or a list of dictionaries,
-		# and insert it into the queue to be available to the _outflux method.
-		self._output_queue.put_nowait([context, event])
 
+		self._output_queue.put_nowait([context, event])
 
 	def rotate(self):
 		'''
