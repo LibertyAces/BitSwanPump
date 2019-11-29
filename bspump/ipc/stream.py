f55e0e9bdc3c896bfa7596d9a701c4283cf4cd97
Jakub Boukal <www.bagr@gmail.com>
2019-11-29 19:11:08 +0100

    PLUM-933: Add health check for StreamSink
    http://jira.teskalabs.int:8080/browse/PLUM-933

--- bspump/ipc/stream.py
+++ bspump/ipc/stream.py
@@ -93,33 +93,44 @@ class StreamSink(Sink):
 	def __init__(self, app, pipeline, id=None, config=None):
 		super().__init__(app, pipeline, id=id, config=config)
 
-		self.Writer = None
+		self.Writer = self.Reader = None
 		self.Pipeline.throttle(self, enable=True)
 
 		self.Address = str(self.Config['address'])
 		self.Pipeline.PubSub.subscribe("bspump.pipeline.start!", self._open_connection)
 		self.Pipeline.PubSub.subscribe("bspump.pipeline.stop!", self._close_connection)
+		app.PubSub.subscribe("Application.tick!", self._health_check)
 
-	async def _open_connection(self, *_, **__):
+	async def _open_connection(self, message, pipeline):
 		assert self.Writer is None
 		if ":" in self.Address:
 			host, port = self.Address.rsplit(":", maxsplit=1)
 			(family, socktype, proto, canonname, sockaddr) = socket.getaddrinfo(host, port)[0]
 			host, port = sockaddr
-			_reader, self.Writer = await asyncio.open_connection(host, port)
+			self.Reader, self.Writer = await asyncio.open_connection(host, port)
 		else:
-			_reader, self.Writer = await asyncio.open_unix_connection(self.Address)
+			self.Reader, self.Writer = await asyncio.open_unix_connection(self.Address)
 
-		self.Pipeline.throttle(self, enable=False)
+		pipeline.throttle(self, enable=False)
 
-	async def _close_connection(self, *_, **__):
+	async def _close_connection(self, message, pipeline):
+
+		if self.Reader:
+			self.Reader = None
 		if self.Writer:
-			if not self.Writer.is_closing():
-				self.Writer.close()
+			self.Writer.close()
 			self.Writer = None
-			self.Pipeline.throttle(self, enable=True)
+			pipeline.throttle(self, enable=True)
 
 		assert not self.Writer
+
+	async def _health_check(self, message):
+		if self.Reader:
+			if self.Reader.at_eof():
+				L.warning("Connection lost. Closing StreamSink")
+				await self._close_connection(message, self.Pipeline)
+		elif self.Writer is None:
+			await self._open_connection(message, self.Pipeline)
 
 	def process(self, context, event):
 		self.Writer.write(event)
