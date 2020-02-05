047b20646742635af0240852881063cfdda368d6
Adam Wichera <adam.wichera@teskalabs.com>
2020-02-05 13:56:16 +0100

    Fixes abc Lookup to work with new BSPUMP API.

--- bspump/abc/lookup.py
+++ bspump/abc/lookup.py
@@ -51,7 +51,7 @@ class Lookup(abc.ABC, asab.ConfigObject):
 			master_lookup_id = self.Config['master_lookup_id']
 			if master_lookup_id == "":
 				master_lookup_id = self.Id
-			self.MasterURL = master_url + '/lookup/' + master_lookup_id
+			self.MasterURL = master_url + '/bspump/v1/lookup/' + master_lookup_id
 		else:
 			self.MasterURL = None  # No master is defined
 
