3cd9c4c0ee284bb63a566509023e74bc7fa8dcae
Jakub Schier <jakub.schier@mojemana.cz>
2020-02-21 16:43:46 +0100

    Add documentation

--- examples/bspump-mysql-lookup-microservice.py
+++ examples/bspump-mysql-lookup-microservice.py
@@ -1,10 +1,26 @@
 import bspump
-import bspump.web
 import bspump.mysql
+import bspump.web
 import aiohttp.web
 
 
 class MyApplication(bspump.BSPumpApplication):
+	"""
+	Example microservice serving preconfigured MySQL querry.
+	Example setup:
+		- run MySQL in docker: $ docker run --rm -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root_password mysql
+		- connect using: $  mysql -h 127.0.0.1 -P 3306 -u root -p
+		- type password
+		- create database and switch into it:
+			- mysql> create database users;
+			- mysql> use users
+		- create table:
+			- mysql> CREATE TABLE user_loc (id INT NOT NULL AUTO_INCREMENT, user CHAR(30), lat FLOAT(9,6), lon FLOAT(9,6), PRIMARY KEY (id));
+		- create some records:
+			- mysql> INSERT INTO user_loc (user, lat, lon) VALUES ("user_0", 37.405992,-122.078515),("user_1", 50.08804, 14.42076);
+		- run the example
+		- You can now fire a GET request to localhost:8080/query_result
+	"""
 	def __init__(self):
 		super().__init__()
 
@@ -27,6 +43,11 @@ class MyApplication(bspump.BSPumpApplication):
 
 
 def webservice(request):
+	"""
+	Here, we get the bspump service, and from it, we localize the MySQLLookup using their respective id strings.
+	That gives us access to the Lookup, and we are able to serve its contents, using the keys we define, in the
+	response.
+	"""
 	svc = app.get_service("bspump.PumpService")
 	Lookup = svc.locate_lookup("MySQLLookup")
 	defined_keys = ['user_0', 'user_1']
@@ -42,6 +63,6 @@ if __name__ == '__main__':
 	app.add_module(bspump.web.Module)
 	websvc = app.get_service("asab.WebService")
 
-	websvc.WebApp.router.add_get('/querry_result', webservice)
+	websvc.WebApp.router.add_get('/query_result', webservice)
 
 	app.run()
