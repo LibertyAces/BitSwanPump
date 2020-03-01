8f19c74f426a95425ab18bd0c504b4a8f02d7e99
Jakub Boukal <www.bagr@gmail.com>
2020-03-01 03:20:24 +0100

    Add configuration for Kafka SASL

--- bspump/kafka/sink.py
+++ bspump/kafka/sink.py
@@ -74,6 +74,10 @@ class KafkaSink(Sink):
 		"enable_idempotency": "",
 		"transactional_id": "",
 		"transaction_timeout_ms": "",
+		"sasl_mechanism": "",
+		"sasl_plain_username": "",
+		"sasl_plain_password": "",
+		"security_protocol": "",
 	}
 
 
@@ -103,7 +107,11 @@ class KafkaSink(Sink):
 			"connections_max_idle_ms": int,
 			"enable_idempotence": bool,
 			"transactional_id": str,
-			"transaction_timeout_ms": int
+			"transaction_timeout_ms": int,
+			"sasl_mechanism": str,
+			"sasl_plain_username": str,
+			"sasl_plain_password": str,
+			"security_protocol": str,
 		}
 		self._producer_params = {
 			x: producer_param_definition[x](y)
