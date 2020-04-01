2022b230e5c2280a23ef183e6e89305d05dbae43
Přemysl Černý <premysl.cerny@teskalabs.com>
2020-04-01 11:30:59 +0200

    Add declarative builder and abstract class.

---
+++ bspump/declarative/expression/__init__.py
@@ -0,0 +1,29 @@
+from .builder import ExpressionBuilder
+from .abc import Expression
+
+# Arithmetic
+from .arithmetic.addexpr import ADD
+from .arithmetic.divideexpr import DIVIDE
+from .arithmetic.multiplyexpr import MULTIPLY
+from .arithmetic.subtractexpr import SUBTRACT
+
+# Assignment
+from .assignment.assignexpr import ASSIGN
+
+# Logical
+from .logical.andexpr import AND
+from .logical.equalsexpr import EQUALS
+from .logical.highereqexpr import HIGHEREQ
+from .logical.higherexpr import HIGHER
+from .logical.ifexpr import IF
+from .logical.lowereqexpr import LOWEREQ
+from .logical.lowerexpr import LOWER
+from .logical.notexpr import NOT
+from .logical.orexpr import OR
+
+# String
+from .string.joinexpr import JOIN
+
+# Value
+from .value.fieldexpr import FIELD
+from .value.tokenexpr import TOKEN
---
+++ bspump/declarative/expression/builder.py
@@ -0,0 +1,15 @@
+import importlib
+
+from .value.tokenexpr import TOKEN
+
+
+class ExpressionBuilder(object):
+
+	@classmethod
+	def build(cls, expression, module_name="bspump.declarative.expression"):
+		if isinstance(expression, dict):
+			_module = importlib.import_module(module_name)
+			_class = getattr(_module, expression["class"])
+			return _class(expression)
+		else:
+			return TOKEN({"token": expression})
