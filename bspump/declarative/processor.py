6d1c297f208b21d248e79d63573f068a5ebaeaac
Přemysl Černý <premysl.cerny@teskalabs.com>
2020-04-02 09:41:31 +0200

    Add construct method to DeclarativeProcessor.

--- bspump/declarative/processor.py
+++ bspump/declarative/processor.py
@@ -6,6 +6,13 @@ from .expression import ExpressionBuilder
 
 class DeclarativeProcessor(bspump.Processor):
 
+	@classmethod
+	def construct(cls, app, pipeline, definition: dict):
+		_id = definition.get("id")
+		config = definition.get("config")
+		expression = definition.get("expression")
+		return cls(app, pipeline, expression=expression, id=_id, config=config)
+
 	def __init__(self, app, pipeline, expression, expression_class_registry=ExpressionClassRegistry(), id=None, config=None):
 		super().__init__(app, pipeline, id=id, config=config)
 		self.Expression = ExpressionBuilder.build(app, expression_class_registry, expression)
