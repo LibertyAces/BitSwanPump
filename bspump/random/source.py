f574e2c8a051ec57eadea3e4df3f66286f8191c2
Ales Teska <ales.teska@teskalabs.com>
2020-04-04 18:51:36 +0200

    Reducement of the creativity :-)

--- bspump/random/source.py
+++ bspump/random/source.py
@@ -11,16 +11,13 @@ class RandomSource(TriggerSource):
 	'''
 		`RandomSource` is mostly meant for testing. It
 		generates n (specified in `Config` as `number`, default is 1000) events per trigger fire.
-		Those event contain only one field with the name from `Config['field']`, default is "id".
+		
 		There can be 2 options of usage:
 		a) User provides `choice` as an array of values to choice randomly and assigned to field;
-		b) The random integer between `Config['lower_bound']` and `Config['upper_bound']` is generated with
-		`Config['prefix']` and assigned.
+		b) The random integer between `Config['lower_bound']` and `Config['upper_bound']`
 	'''
 
 	ConfigDefaults = {
-		'field': 'id',
-		'prefix': 'id_',
 		'number': 1000,
 		'lower_bound': 0,
 		'upper_bound': 1000,
@@ -30,8 +27,6 @@ class RandomSource(TriggerSource):
 
 	def __init__(self, app, pipeline, choice=None, id=None, config=None):
 		super().__init__(app, pipeline, id=id, config=config)
-		self.Field = self.Config['field']
-		self.Prefix = self.Config['prefix']
 		self.Number = int(self.Config['number'])
 		self.LowerBound = int(self.Config['lower_bound'])
 		self.UpperBound = int(self.Config['upper_bound'])
@@ -43,21 +38,19 @@ class RandomSource(TriggerSource):
 			self.Choice = choice
 
 
-	def generate_random(self, event):
+	def generate_random(self):
 		'''
 			Override this method to generate differently
 		'''
 		if self.Choice is not None:
-			event[self.Field] = random.choice(self.Choice)
+			return random.choice(self.Choice)
 		else:
-			n = random.randint(self.LowerBound, self.UpperBound)
-			event[self.Field] = "{}{}".format(self.Prefix, n)
+			return random.randint(self.LowerBound, self.UpperBound)
 
 
 	async def cycle(self):
 		for i in range(0, self.Number):
-			event = {}
-			self.generate_random(event)
+			event = self.generate_random()
 			await self.process(event)
 			if self.EventCounter >= self.EventsTillIdle:
 				await asyncio.sleep(self.EventIdleTime)
