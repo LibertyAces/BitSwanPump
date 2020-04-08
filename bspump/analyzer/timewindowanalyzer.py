7ae66278d105445835dc434c61f79b426ba2d05a
Ales Teska <ales.teska@teskalabs.com>
2020-04-09 00:26:41 +0200

    Facelift.

--- bspump/analyzer/timewindowanalyzer.py
+++ bspump/analyzer/timewindowanalyzer.py
@@ -17,32 +17,7 @@ class TimeWindowAnalyzer(Analyzer):
 		Timer periodically shifts the window by time window resolution, dropping previous events.
 
 		`TimeWindowAnalyzer` operates over the `TimeWindowMatrix` object.
-		`tw_dimensions` is matrix dimensions parameter as the tuple `(column_number, third_dimension)`.
-		Example: `(5,1)` will create the matrix with n rows, 5 columns and 1 additional third dimension.
-		`tw_format` is the letter from the table + number:
 
-			+------------+------------------+
-			| Name       | Definition       |
-			+============+==================+
-			| 'b'        | Byte             |
-			+------------+------------------+
-			| 'i'        | Signed integer   |
-			+------------+------------------+
-			| 'u'        | Unsigned integer |
-			+------------+------------------+
-			| 'f'        | Floating point   |
-			+------------+------------------+
-			| 'c'        | Complex floating |
-			|            | point            |
-			+------------+------------------+
-			| 'S'        | String           |
-			+------------+------------------+
-			| 'U'        | Unicode string   |
-			+------------+------------------+
-			| 'V'        | Raw data         |
-			+------------+------------------+
-
-		Example: 'i8' stands for int64.
 		`resolution`is how many seconds fit in one time cell, default value is `60`.
 		`start_time` is a unix timestamp for time to start. Default value is `None`, which will be equivalent current time.
 		`clock_driven` is a boolean parameter, specifying how the matrix should be advanced. If `True`, it advances on timer's tick,
@@ -50,14 +25,10 @@ class TimeWindowAnalyzer(Analyzer):
 		`matrix_id` is an id of `TimeWindowMatrix` object alternatively passed, if not provided, the new matrix will be created with and ID derived from the Analyzer Id
 		`analyze_on_clock` enables enables analyzis by timer.
 
-
 		If the `TimeWindowAnalyzer` is `clock_driven`, the time should be periodically shifted (`on_clock_tick()`). The same
 		function runs analyzis, if it's enabled.
 	'''
 
-	ConfigDefaults = {
-		'resolution': 60,  # Resolution (aka column width) in seconds
-	}
 
 	def __init__(
 		self, app, pipeline, matrix_id=None, dtype='float_',
