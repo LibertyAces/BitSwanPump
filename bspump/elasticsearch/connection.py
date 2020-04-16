c52f0cb76f2e88d32bd7148bdf6fe0ba1c29beca
Přemysl Černý <premysl.cerny@teskalabs.com>
2020-04-16 12:06:51 +0200

    Use init_value in counter.

--- bspump/elasticsearch/connection.py
+++ bspump/elasticsearch/connection.py
@@ -102,13 +102,7 @@ class ElasticSearchConnection(Connection):
 
 		# Initialize metrics
 		metrics_service = app.get_service('asab.MetricsService')
-		self.BulkInsertReturnCodesCounter = metrics_service.create_counter("esconnection.bulkinsert.returncodes", init_values={
-			"400": 0,
-			"404": 0,
-			"409": 0,
-			"500": 0,
-			"unknown": 0,
-		})
+		self.BulkInsertReturnCodesCounter = metrics_service.create_counter("esconnection.bulkinsert.returncodes", init_values=dict())
 
 	def get_url(self):
 		return random.choice(self.node_urls)
@@ -206,10 +200,7 @@ class ElasticSearchConnection(Connection):
 						if respj.get('errors', True) is not False:
 							error_level = 0
 							for item in respj['items']:
-								try:
-									self.BulkInsertReturnCodesCounter.add(item['index']['status'], 1)
-								except KeyError:
-									self.BulkInsertReturnCodesCounter.add("unknown", 1)
+								self.BulkInsertReturnCodesCounter.add(str(item['index']['status']), 1, init_value=0)
 								if item['index']['status'] not in self.AllowedBulkResponseCodes:
 									if error_level == 0:
 										L.error("Failed to insert bulk into ElasticSearch status: {}".format(resp.status))
