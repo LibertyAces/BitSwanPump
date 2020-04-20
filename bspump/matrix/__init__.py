6bcfa5da7aa7932ec26f67059fc90deefb31b402
margirova <margarita.argirova@teskalabs.com>
2020-04-20 11:54:55 +0200

    another import fix

--- bspump/matrix/__init__.py
+++ bspump/matrix/__init__.py
@@ -1,8 +1,8 @@
 from .matrixexportcsvgenerator import SessionMatrixExportCSVGenerator, TimeWindowMatrixExportCSVGenerator
 from .source import MatrixSource
 from .matrix import Matrix, PersistentMatrix
-from .namedmatrix import NamedMatrix, PersistentNamedMatrix
-from .timewindowmatrix import TimeWindowMatrix, PersistentTimeWindowMatrix
+from .namedmatrix import NamedMatrix, PersistentNamedMatrix  # noqa: F401
+from .timewindowmatrix import TimeWindowMatrix, PersistentTimeWindowMatrix  # noqa: F401
 from .sessionmatrix import SessionMatrix, PersistentSessionMatrix
 from .geomatrix import GeoMatrix, PersistentGeoMatrix
 
