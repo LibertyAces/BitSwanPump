9826e3a13f8ee7c1bf9019d4e7e51cceb37a1ccc
margirova <margarita.argirova@teskalabs.com>
2020-04-20 11:53:30 +0200

    fixed imports

--- bspump/matrix/__init__.py
+++ bspump/matrix/__init__.py
@@ -2,23 +2,23 @@ from .matrixexportcsvgenerator import SessionMatrixExportCSVGenerator, TimeWindo
 from .source import MatrixSource
 from .matrix import Matrix, PersistentMatrix
 from .namedmatrix import NamedMatrix, PersistentNamedMatrix
-# from .timewindowmatrix import TimeWindowMatrix, PersistentTimeWindowMatrix
+from .timewindowmatrix import TimeWindowMatrix, PersistentTimeWindowMatrix
 from .sessionmatrix import SessionMatrix, PersistentSessionMatrix
 from .geomatrix import GeoMatrix, PersistentGeoMatrix
 
 
-__all__ = (
+__all__ = [
 	'SessionMatrixExportCSVGenerator',
 	'TimeWindowMatrixExportCSVGenerator',
 	'MatrixSource',
 	'Matrix',
 	'PersistentMatrix',
-	# 'NamedMatrix',
-	# 'PersistentNamedMatrix'
-	# 'TimeWindowMatrix',
-	# 'PersistentTimeWindowMatrix',
+	'NamedMatrix',
+	'PersistentNamedMatrix'
+	'TimeWindowMatrix',
+	'PersistentTimeWindowMatrix',
 	'SessionMatrix',
 	'PersistentSessionMatrix',
 	'GeoMatrix',
 	'PersistentGeoMatrix',
-)
+]
