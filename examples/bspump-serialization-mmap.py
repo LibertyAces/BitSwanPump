f47e8f882cc0d1af5e3906c30cf10c6f6bfed15b
margirova <margarita.argirova@teskalabs.com>
2020-04-20 19:35:48 +0200

    removed prints from example

--- examples/bspump-serialization-mmap.py
+++ examples/bspump-serialization-mmap.py
@@ -32,18 +32,18 @@ class MyPipeline(Pipeline):
 		lb = ub - 22
 		self.build(
 			bspump.random.RandomStructuredSource(app, self,
-				config={'number': 30, 'upper_bound': 7, 'field': 'id', 'prefix': ''}
+				config={'number': 30, 'upper_bound': 1000, 'field': 'id', 'prefix': ''}
 				).on(bspump.trigger.OpportunisticTrigger(app, chilldown_period=1)),
 			bspump.random.RandomEnricher(app, self, config={'field':'duration', 'lower_bound':1, 'upper_bound': 5}, id="RE0"),
 			bspump.random.RandomEnricher(app, self, config={'field': '@timestamp', 'lower_bound': lb, 'upper_bound': ub}, id="RE1"),
 			bspump.random.RandomEnricher(app, self, choice=['abc', 'cde', 'efg'], config={'field':'user'}, id="RE2"),
 			bspump.random.RandomEnricher(app, self, choice=[{'lat': 50, 'lon': 14}, {'lat':52, 'lon': 13}, {'lat': 48, 'lon': 17}], config={'field':'L'}, id="RE3"),
-			# MySessionAnalyzer(app, self, dtype=[('user', 'U10'), ('duration', 'i8')], analyze_on_clock=True, persistent=True,
+			# MySessionAnalyzer(app, self, dtype=[('user', 'U10'), ('duration', 'i8')], analyze_on_clock=True, persistent=False,
 			# 	config={'analyze_period': 5, 'path':'examples/mmap/sessions'}), 
-			MyTimeWindowAnalyzer(app, self, columns=10, resolution=2, clock_driven=True, analyze_on_clock=True, persistent=True,
-				config={'analyze_period': 5, 'path': 'examples/mmap/timewindow'}),
-			# MyGeoAnalyzer(app, self, resolution=10, analyze_on_clock=True, persistent=True,
-			# 	config={'analyze_period': 5, 'path': 'examples/mmap/geo'}),
+			# MyTimeWindowAnalyzer(app, self, columns=10, resolution=2, clock_driven=True, analyze_on_clock=True, persistent=False,
+			# 	config={'analyze_period': 5, 'path': 'examples/mmap/timewindow'}),
+			MyGeoAnalyzer(app, self, resolution=10, analyze_on_clock=True, persistent=False,
+				config={'analyze_period': 5, 'path': 'examples/mmap/geo'}),
 			NullSink(app, self)
 		)
 
@@ -61,24 +61,9 @@ class MySessionAnalyzer(SessionAnalyzer):
 
 	
 	def analyze(self):
-		print("BEFORE")
-		print("???", self.Sessions.Array)
-		print("!!", self.Sessions.Index.serialize())
-		print("77&7", self.Sessions.ClosedRows.serialize())
-
 		self.Sessions.close_row('2')
-		print("???", self.Sessions.Array)
 		self.Sessions.close_row('8')
-		print("AFTER (0)")
-		print("???", self.Sessions.Array)
-		print("!!", self.Sessions.Index.serialize())
-		print("77&7", self.Sessions.ClosedRows.serialize())
-		# self.App.stop()
-		# self.Sessions.close_rows(['4', '2', '7'])
-		# print("AFTER(1)")
-		# print("???", self.Sessions.Array)
-		# print("!!", self.Sessions.Index.serialize())
-		# print("77&7", self.Sessions.ClosedRows.serialize())
+
 
 
 class MyTimeWindowAnalyzer(TimeWindowAnalyzer):	
@@ -97,38 +82,9 @@ class MyTimeWindowAnalyzer(TimeWindowAnalyzer):
 
 	
 	def analyze(self):
-		print("BEFORE")
-		print("???", self.TimeWindow.Array)
-		print("!!", self.TimeWindow.Index.serialize())
-		print("**", self.TimeWindow.Index.Map)
-		print("___", self.TimeWindow.ClosedRows.CR)
-		print("---", self.TimeWindow.ClosedRows.CRBit)
-		print("$$$$$$", self.TimeWindow.TimeConfig.TC)
-		print("^^^^^^", self.TimeWindow.WarmingUpCount.WUC)
 		self.TimeWindow.close_row('2')
 		self.TimeWindow.close_row('5')
-		print("AFTER (0)")
-		print("???", self.TimeWindow.Array)
-		print("!!", self.TimeWindow.Index.serialize())
-		print("**", self.TimeWindow.Index.Map)
-		print("___", self.TimeWindow.ClosedRows.CR)
-		print("---", self.TimeWindow.ClosedRows.CRBit)
-		print("$$$$$$", self.TimeWindow.TimeConfig.TC)
-		print("^^^^^^", self.TimeWindow.WarmingUpCount.WUC)
-		print("###########")
-		print("###########")
-		print("###########")
 		self.TimeWindow.close_rows(['4', '2', '6'])
-		print("###########", self.TimeWindow.ClosedRows.CR)
-		print("AFTER(1)")
-		print("???", self.TimeWindow.Array)
-		print("!!", self.TimeWindow.Index.serialize())
-		print("**", self.TimeWindow.Index.Map)
-		print("___", self.TimeWindow.ClosedRows.CR)
-		print("---", self.TimeWindow.ClosedRows.CRBit)
-		print("$$$$$$", self.TimeWindow.TimeConfig.TC)
-		print("^^^^^^", self.TimeWindow.WarmingUpCount.WUC)
-		self.App.stop()
 
 
 class MyGeoAnalyzer(GeoAnalyzer):
@@ -139,7 +95,6 @@ class MyGeoAnalyzer(GeoAnalyzer):
 		self.GeoMatrix.Array[x, y] += 1
 
 	def analyze(self):
-		print("Hey")
 		coordinates = np.where(self.GeoMatrix.Array != 0)
 		xx = coordinates[0]
 		yy = coordinates[1]
