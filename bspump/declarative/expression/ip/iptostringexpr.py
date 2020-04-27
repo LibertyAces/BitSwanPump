a71d7085dde1e999b190e0f5a105d74d47b8fbdd
Přemysl Černý <premysl.cerny@teskalabs.com>
2020-04-27 11:54:37 +0200

    Introduce IP, IPTOSTRING expressions and the IP module. Move INSUBNET, make it list.

---
+++ bspump/declarative/expression/ip/iptostringexpr.py
@@ -0,0 +1,17 @@
+from netaddr import IPAddress
+
+from bspump.declarative.abc import Expression
+
+
+class IPTOSTRING(Expression):
+	"""
+	Parses IP address to its string representation.
+	"""
+
+	def __init__(self, app, *, value):
+		super().__init__(app)
+		self.Value = value
+
+	def __call__(self, context, event, *args, **kwargs):
+		ip = IPAddress(self.evaluate(self.Value, context, event, *args, **kwargs))
+		return str(ip)
