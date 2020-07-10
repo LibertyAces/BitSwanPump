6ef71577cc1f166cff80876d28be37c791061bd2
Jakub Mach <jakub.m4ch@gmail.com>
2020-07-10 04:04:59 +0200

    Implemented PostgreSQLLookup

--- bspump/postgresql/__init__.py
+++ bspump/postgresql/__init__.py
@@ -1,10 +1,11 @@
 from .connection import PostgreSQLConnection
 from .sink import PostgreSQLSink
 from .logicalreplicationsource import PostgreSQLLogicalReplicationSource
-
+from .lookup import PostgreSQLLookup
 
 __all__ = (
 	'PostgreSQLConnection',
 	'PostgreSQLSink',
 	'PostgreSQLLogicalReplicationSource',
+	'PostgreSQLLookup',
 )
