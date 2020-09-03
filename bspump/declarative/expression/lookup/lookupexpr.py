ecf0b29adffedc5850ed1bbd96cd477ee89c81e1
Ales Teska <ales.teska@teskalabs.com>
2020-09-03 13:08:29 +0200

    Explicit compound key support for declarative language.

--- bspump/declarative/expression/lookup/lookupexpr.py
+++ bspump/declarative/expression/lookup/lookupexpr.py
@@ -9,11 +9,25 @@ class LOOKUP_GET(Expression):
 		self.Lookup = svc.locate_lookup(arg_in)
 		self.Key = arg_what
 
+
+	def build_key(self, context, event, *args, **kwargs):
+		if isinstance(self.Key, (list, tuple)):
+			# Compound key
+			return tuple(
+				evaluate(k, context, event, *args, **kwargs) for k in self.Key
+			)
+		else:
+			# Simple key
+			return evaluate(self.Key, context, event, *args, **kwargs)
+
+
 	def __call__(self, context, event, *args, **kwargs):
-		return self.Lookup.get(evaluate(self.Key, context, event, *args, **kwargs))
+		key = self.build_key(context, event, *args, **kwargs)
+		return self.Lookup.get(key)
 
 
 class LOOKUP_CONTAINS(LOOKUP_GET):
 
 	def __call__(self, context, event, *args, **kwargs):
-		return super().__call__(context, event, *args, **kwargs) is not None
+		key = self.build_key(context, event, *args, **kwargs)
+		return key in self.Lookup
