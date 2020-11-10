bb9cb3f02905d70bdbe9c188855579607106b583
Ales Teska <ales.teska@teskalabs.com>
2020-11-10 23:57:16 +0100

    Initial work related to #78

--- bspump/elasticsearch/connection.py
+++ bspump/elasticsearch/connection.py
@@ -39,68 +39,75 @@ class ElasticSearchBulk(object):
 
 		url = url + '{}/_bulk?filter_path={}'.format(self.Index, self.FilterPath)
 
-		async with session.post(
-			url,
-			data=self._data_feeder(),
-			headers={
-				'Content-Type': 'application/json'
-			},
-			timeout=timeout,
-		) as resp:
+		try:
+			resp = await session.post(
+				url,
+				data=self._data_feeder(),
+				headers={
+					'Content-Type': 'application/json'
+				},
+				timeout=timeout,
+			)
+		except OSError as e:
+			L.warn("{}".format(e))
+			return False
 
-			# Obtain the response from ElasticSearch,
-			# which should always be a json
-			resp_body = await resp.json()
+		# Obtain the response from ElasticSearch,
+		# which should always be a json
+		resp_body = await resp.json()
 
-			if resp.status == 200:
+		if resp.status == 200:
 
-				# Check that all documents were successfully inserted to ElasticSearch
-				# If there are no error messages, continue
-				if not resp_body.get("errors", False):
-					self.InsertMetric.add("ok", items_count)
-					return
+			# Check that all documents were successfully inserted to ElasticSearch
+			# If there are no error messages, continue
+			if not resp_body.get("errors", False):
+				self.InsertMetric.add("ok", items_count)
+				return True
 
-				# Some of the documents were not inserted properly,
-				# usually because of attributes mismatch, see:
-				# https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html
+			# Some of the documents were not inserted properly,
+			# usually because of attributes mismatch, see:
+			# https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html
 
-				response_items = resp_body.get("items")
-				self.partial_error_callback(response_items)
+			response_items = resp_body.get("items")
+			self.partial_error_callback(response_items)
 
-				# When the log handling is not precise,
-				# the iteration only happens on error items natively
-				# Log first 20 errors
-				counter = 0
-				for response_item in response_items:
-					if "error" not in response_item.get("index", ""):
-						continue
+			# When the log handling is not precise,
+			# the iteration only happens on error items natively
+			# Log first 20 errors
+			counter = 0
+			for response_item in response_items:
+				if "error" not in response_item.get("index", ""):
+					continue
 
-					if counter < self.FailLogMaxSize:
-						L.error("Failed to insert document into ElasticSearch: '{}'".format(
-							str(response_item)
-						))
-
-					counter += 1
-
-				# Show remaining log messages
-				if counter > self.FailLogMaxSize:
-					L.error("Failed to insert document into ElasticSearch: '{}' more insertions of documents failed".format(
-						counter - self.FailLogMaxSize
+				if counter < self.FailLogMaxSize:
+					L.error("Failed to insert document into ElasticSearch: '{}'".format(
+						str(response_item)
 					))
 
-				# Insert metrics
-				self.InsertMetric.add("fail", counter)
-				self.InsertMetric.add("ok", items_count - counter)
+				counter += 1
 
-			else:
-
-				# An ElasticSearch error occurred while inserting documents
-				self.InsertMetric.add("fail", items_count)
-				L.error("Failed to insert document into ElasticSearch status:{} body:{}".format(
-					resp.status,
-					resp_body
+			# Show remaining log messages
+			if counter > self.FailLogMaxSize:
+				L.error("Failed to insert document into ElasticSearch: '{}' more insertions of documents failed".format(
+					counter - self.FailLogMaxSize
 				))
-				self.full_error_callback(self.Items, resp.status)
+
+			# Insert metrics
+			self.InsertMetric.add("fail", counter)
+			self.InsertMetric.add("ok", items_count - counter)
+
+		else:
+
+			# An ElasticSearch error occurred while inserting documents
+			self.InsertMetric.add("fail", items_count)
+			L.error("Failed to insert document into ElasticSearch status:{} body:{}".format(
+				resp.status,
+				resp_body
+			))
+			return self.full_error_callback(self.Items, resp.status)
+
+		return True
+
 
 	async def _data_feeder(self):
 		for _id, data in self.Items:
@@ -116,7 +123,6 @@ class ElasticSearchBulk(object):
 		:param response_items: list with dict items: {"index": {"_id": ..., "error": ...}}
 		:return:
 		"""
-		pass
 
 	def full_error_callback(self, bulk_items, return_code):
 		"""
@@ -124,9 +130,9 @@ class ElasticSearchBulk(object):
 		this callback is called.
 		:param bulk_items: list with tuple items: (_id, data)
 		:param return_code: ElasticSearch return code
-		:return:
+		:return: False if the bulk is to be resumbitted again
 		"""
-		pass
+		return True
 
 
 class ElasticSearchConnection(Connection):
@@ -252,19 +258,22 @@ class ElasticSearchConnection(Connection):
 		if bulk.consume(_id, data):
 			# Bulk is ready, schedule to be send
 			del self._bulks[index]
-			self._output_queue.put_nowait(bulk)
-
-			# Signalize need for throttling
-			if self._output_queue.qsize() == self._output_queue_max_size:
-				self.PubSub.publish("ElasticSearchConnection.pause!", self)
+			self.enqueue(bulk)
 
 	def _start(self, event_type):
 		self.PubSub.subscribe("Application.tick!", self._on_tick)
 		self._on_tick("simulated!")
 
 	async def _on_exit(self, event_name):
-		self._started = False
+		# Wait till the queue is empty
 		self.flush(forced=True)
+		while self._output_queue.qsize() > 0:
+			self.flush(forced=True)
+			await asyncio.sleep(1)
+			if self._output_queue.qsize() > 0:
+				L.warn("Still have {} bulk in output queue".format(self._output_queue.qsize()))
+
+		self._started = False
 
 		# Wait till the _loader() terminates (one after another)
 		pending = [item[1] for item in self._futures]
@@ -272,7 +281,8 @@ class ElasticSearchConnection(Connection):
 		while len(pending) > 0:
 			# By sending None via queue, we signalize end of life
 			await self._output_queue.put(None)
-			done, pending = await asyncio.wait(pending, return_when=asyncio.ALL_COMPLETED)
+			done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
+
 
 	def _on_tick(self, event_name):
 		self.QueueMetric.set("size", int(self._output_queue.qsize()))
@@ -310,14 +320,41 @@ class ElasticSearchConnection(Connection):
 
 		for index in aged:
 			bulk = self._bulks.pop(index)
-			self._output_queue.put_nowait(bulk)
+			self.enqueue(bulk)
 
-			# Signalize need for throttling
-			if self._output_queue.qsize() == self._output_queue_max_size:
-				self.PubSub.publish("ElasticSearchConnection.pause!", self)
+	def enqueue(self, bulk):
+		'''
+		Properly enqueue the bulk.
+		'''
+		self._output_queue.put_nowait(bulk)
+
+		# Signalize need for throttling
+		if self._output_queue.qsize() == self._output_queue_max_size:
+			self.PubSub.publish("ElasticSearchConnection.pause!", self)
 
 	async def _loader(self, url):
 		async with self.get_session() as session:
+
+			# Preflight check
+			try:
+				async with session.get(url + "_cluster/health") as resp:
+					await resp.json()
+					if resp.status != 200:
+						L.warn("Cluster is not ready".format(resp.status))
+						await asyncio.sleep(5)  # Throttle a bit before next try
+						return
+
+			except OSError as e:
+				L.warn("{}".format(e))
+				await asyncio.sleep(5)  # Throttle a bit before next try
+				return
+
+			except aiohttp.client_exceptions.ContentTypeError as e:
+				L.warn("Failed communication {}".format(e))
+				await asyncio.sleep(20)  # Throttle a bit before next try
+				return
+
+			# Push buld into the ElasticSearch
 			while self._started:
 				bulk = await self._output_queue.get()
 				if bulk is None:
@@ -326,4 +363,9 @@ class ElasticSearchConnection(Connection):
 				if self._output_queue.qsize() == self._output_queue_max_size - 1:
 					self.PubSub.publish("ElasticSearchConnection.unpause!", self)
 
-				await bulk.upload(url, session, self._timeout)
+				sucess = await bulk.upload(url, session, self._timeout)
+				if not sucess:
+					# Requeue the bulk for another delivery attempt to ES
+					self.enqueue(bulk)
+					await asyncio.sleep(5)  # Throttle a bit before next try
+					break  # Exit the loader (new will be started automatically)
