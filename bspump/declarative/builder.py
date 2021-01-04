a10f36b78997025b3272f8591e22114ceb31d87d
Přemysl Černý <premysl.cerny@teskalabs.com>
2021-01-04 15:50:53 +0100

    Do not optimize list of expressions.

--- bspump/declarative/builder.py
+++ bspump/declarative/builder.py
@@ -135,32 +135,8 @@ class ExpressionBuilder(object):
 							break
 
 				elif isinstance(expression, list):
-
-					for _expression in expression:
-
-						# Skip nested dicts (like triggers, evaluate sections etc.)
-						if isinstance(_expression, (dict, list)):
-							continue
-
-						# Walk the syntax tree
-						for parent, key, obj in _expression.walk():
-							if not isinstance(obj, Expression):
-								continue
-
-							# Check if the node could be optimized
-							opt_obj = obj.optimize()
-							if opt_obj is None:
-								continue
-
-							if parent is None:
-								expression[key] = opt_obj
-							else:
-								# If yes, replace a given node by the optimized variant
-								parent.set(key, opt_obj)
-
-							# ... and start again
-							retry = True
-							break
+					# We are not optimizing lists of expressions now
+					continue
 
 				else:
 
