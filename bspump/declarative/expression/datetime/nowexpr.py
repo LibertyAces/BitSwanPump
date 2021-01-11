f5552de64b3d610d6185004df08ab5da2980f85f
Ales Teska <ales.teska@teskalabs.com>
2021-01-11 14:08:50 +0100

    Add an outlet type for `!IN`

--- bspump/declarative/expression/datetime/nowexpr.py
+++ bspump/declarative/expression/datetime/nowexpr.py
@@ -23,9 +23,15 @@ class NOW(Expression):
 
 	Attributes = {}
 
+
 	def __init__(self, app, *, value):
 		super().__init__(app)
 		assert(value == "")
 
+
 	def __call__(self, context, event, *args, **kwargs):
 		return self.Datetime.utcnow().timestamp()
+
+
+	def get_outlet_type(self):
+		return float.__name__
