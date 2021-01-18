2c7128184a6de36dce493f7a9993bc8e64425461
Ales Teska <ales.teska@teskalabs.com>
2021-01-18 01:12:29 +0100

    Extends an ability to generate DOT to ASTs with YAML alias.

--- bspump/declarative/dot.py
+++ bspump/declarative/dot.py
@@ -1,11 +1,9 @@
-import itertools
-
 from .abc import Expression, SequenceExpression
 
 
 def declaration_to_dot(decl, fname):
 	nodes = set()
-	vidgen = itertools.count(1)
+	links = set()
 
 	with open(fname, "w") as fo:
 		fo.write("digraph G {\n\trankdir=LR;\n")
@@ -17,16 +15,26 @@ def declaration_to_dot(decl, fname):
 			nodes.add(parent)
 
 			if isinstance(obj, Expression):
-				fo.write("\t{} -> {} [label=\"{}\"];\n".format(parent.Id, obj.Id, key))
-				nodes.add(obj)
+				if (parent.Id, obj.Id, key) not in links:
+					if obj in nodes:
+						style = "dashed"  # Link by alias
+					else:
+						style = "filled"
+
+					fo.write("\t\"{}\" -> \"{}\" [style={},label=\"{}\"];\n".format(parent.Id, obj.Id, style, key))
+					nodes.add(obj)
+					links.add((parent.Id, obj.Id, key))
+
 			else:
-				vid = 'V{}'.format(next(vidgen))
-				fo.write("\t{} -> {} [label=\"{}\"];\n".format(parent.Id, vid, key))
-				fo.write("\t{} [shape=box,label=\"<{}>\\n{}\"]\n".format(
-					vid,
-					obj.__class__.__name__,
-					obj
-				))
+				vid = '.{}.val'.format(parent.Id)
+				if (parent.Id, vid, key) not in links:
+					fo.write("\t\"{}\" -> \"{}\" [label=\"{}\"];\n".format(parent.Id, vid, key))
+					fo.write("\t\"{}\" [shape=box,label=\"<{}>\\n{}\"]\n".format(
+						vid,
+						obj.__class__.__name__,
+						obj
+					))
+					links.add((parent.Id, vid, key))
 
 		for obj in nodes:
 			if isinstance(obj, SequenceExpression):
@@ -34,7 +42,7 @@ def declaration_to_dot(decl, fname):
 			else:
 				shape = "octagon"
 
-			fo.write("\t{} [label=\"<{}>\\n{}\",shape=\"{}\"];\n".format(
+			fo.write("\t\"{}\" [label=\"<{}>\\n{}\",shape=\"{}\"];\n".format(
 				obj.Id,
 				obj.get_outlet_type(),
 				obj.__class__.__name__,
