2206193331cf0c0f6b78e826bc0d8d6104ec3526
Ales Teska <ales.teska@teskalabs.com>
2021-01-25 14:55:07 +0100

    Optimisation of '!IN !EVENT'

--- bspump/declarative/expression/test/inexpr.py
+++ bspump/declarative/expression/test/inexpr.py
@@ -1,6 +1,7 @@
 from ...abc import Expression
 
 from ..value.valueexpr import VALUE
+from ..value.eventexpr import EVENT
 
 
 class IN(Expression):
@@ -9,8 +10,8 @@ class IN(Expression):
 	"""
 
 	Attributes = {
-		"What": ["*"],  # TODO: This ...
-		"Where": ["*"],  # TODO: This ...
+		"What": ["*"],  # TODO: 'What' is different from list, set and dict
+		"Where": ["list", "set", "dict"],
 	}
 
 	def __init__(self, app, *, arg_what, arg_where):
@@ -26,17 +27,24 @@ class IN(Expression):
 		else:
 			self.Where = VALUE(app, value=arg_where)
 
-		assert(self.Where.get_outlet_type() in ('list', 'set'))
+		assert(self.Where.get_outlet_type() in ('list', 'set', 'dict'))
 
 
 	def optimize(self):
 		if isinstance(self.Where, VALUE):
+
 			if self.Where.get_outlet_type() == 'list':
 				return IN_optimized_list_where(self)
+
 			if self.Where.get_outlet_type() == 'set':
 				return IN_optimized_set_where(self)
 
-		return self
+
+		print("IN: ", self.What, self.Where)
+		if isinstance(self.What, VALUE) and isinstance(self.Where, EVENT):
+			return IN_optimized_EVENT_VALUE(self)
+
+		return None
 
 
 	def get_items_inlet_type(self):
@@ -109,3 +117,22 @@ class IN_optimized_set_where(IN):
 
 	def __call__(self, context, event, *args, **kwargs):
 		return self.What(context, event, *args, **kwargs) in self._where_value
+
+
+class IN_optimized_EVENT_VALUE(IN):
+
+	def __init__(self, orig):
+		super().__init__(
+			orig.App,
+			arg_what=orig.What,
+			arg_where=orig.Where
+		)
+
+		self._what_value = self.What({}, {})
+
+	def optimize(self):
+		# This is to prevent re-optimising the class
+		return None
+
+	def __call__(self, context, event, *args, **kwargs):
+		return self._what_value in event
