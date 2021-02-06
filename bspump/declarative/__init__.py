e27f2e0e9f9144bf66cfd8d0f9d06f57e216f84b
Ales Teska <ales.teska@teskalabs.com>
2021-02-06 14:57:03 +0100

    Moving a library as well.

--- bspump/declarative/__init__.py
+++ bspump/declarative/__init__.py
@@ -1,39 +1,11 @@
 from .processor import DeclarativeProcessor
 from .generator import DeclarativeGenerator
 from .timewindowanalyzer import DeclarativeTimeWindowAnalyzer
-
-from .builder import ExpressionBuilder
-from .optimizer import ExpressionOptimizer
-from .declerror import DeclarationError
 from .segmentbuilder import SegmentBuilder
-
-from .libraries import DeclarationLibrary
-from .libraries import FileDeclarationLibrary
-from .libraries import ZooKeeperDeclarationLibrary
-
-from .abc import Expression
-from .abc import SequenceExpression
-
-from .dot import declaration_to_dot, declaration_to_dot_stream
 
 __all__ = [
 	"DeclarativeProcessor",
 	"DeclarativeGenerator",
 	"DeclarativeTimeWindowAnalyzer",
-
-	"ExpressionBuilder",
-	"DeclarationError",
 	"SegmentBuilder",
-
-	"ExpressionOptimizer",
-
-	"DeclarationLibrary",
-	"FileDeclarationLibrary",
-	"ZooKeeperDeclarationLibrary",
-
-	"Expression",
-	"SequenceExpression",
-
-	"declaration_to_dot",
-	"declaration_to_dot_stream",
 ]
