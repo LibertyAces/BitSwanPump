2bba044683b7ddacc8a0c607062be6748b4a09ef
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-02-10 09:59:13 +0100

    Fix changestreamsource extention

--- bspump/mongodb/__init__.py
+++ bspump/mongodb/__init__.py
@@ -1,13 +1,13 @@
-from .changestreamsource import MongoDBChangeStreamSource
 from .connection import MongoDBConnection
 from .source import MongoDBSource
 from .lookup import MongoDBLookup
 from .sink import MongoDBSink
+from .changestreamsource import MongoDBChangeStreamSource
 
 __all__ = (
+	'MongoDBChangeStreamSource',
 	'MongoDBConnection',
 	'MongoDBLookup',
 	'MongoDBSource',
-	'MongoDBChangeStreamSource',
 	'MongoDBSink',
 )
