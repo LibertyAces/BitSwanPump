d696ef7762ad1240a2d3462434c8ea4a0ee28a64
rhruska <robin.hruska@teskalabs.com>
2021-02-25 11:46:21 +0100

    use proactor in file provider

--- bspump/lookup/provider/file.py
+++ bspump/lookup/provider/file.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+import asab.proactor
+
 from .abc import LookupBatchProviderABC
 
 ###
@@ -17,8 +19,16 @@ class FileBatchProvider(LookupBatchProviderABC):
 
 	def __init__(self, lookup, url, id=None, config=None):
 		super().__init__(lookup, url, id, config)
+		self.App.add_module(asab.proactor.Module)
+		self.ProactorService = self.App.get_service("asab.ProactorService")
 
 	async def load(self):
+		result = await self.ProactorService.execute(
+			self.load_on_thread,
+		)
+		return result
+
+	def load_on_thread(self):
 		if not os.path.isfile(self.URL):
 			return None
 		if not os.access(self.URL, os.R_OK):
