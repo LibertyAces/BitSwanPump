7a9b0aee8cb98e4d6db396091fab28ee80182da9
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-03-18 10:32:46 +0100

    Move serializer and de serializer to dedicated modules

--- examples/bspump-avro-serialization.py
+++ examples/bspump-avro-serialization.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 import logging
-import json
-import io
-
-import fastavro
-
+from bspump.avro.serializer import AvroSerializer
+from bspump.avro.deserializer import AvroDeserializer
 import bspump
 import bspump.avro
 import bspump.common
@@ -17,73 +14,6 @@ L = logging.getLogger(__name__)
 
 ###
 
-
-# Move this into a dedicate module and use it in Source, Sink, Serialized, Deserializer
-def load_avro_schema(config):
-	schema = config.get('schema')
-	if schema == '':
-		schema_file = config.get('schema_file')
-		if schema_file != '':
-			with open(schema_file, 'r') as fi:
-				schema = json.load(fi)
-
-	if schema == '':
-		raise RuntimeError("Avro scheme not read")
-
-	return fastavro.parse_schema(schema)
-
-
-class AvroSerializer(bspump.Generator):
-
-	ConfigDefaults = {
-		'schema': '',
-		'schema_file': '',  # Used if 'schema is not present'
-		'max_block_size': 10,
-	}
-
-	# TODO: Flush of the Records (in regular time interval and before the processor is finished)
-
-	def __init__(self, app, pipeline, id=None, config=None):
-		super().__init__(app, pipeline, id=id, config=config)
-		self.Schema = load_avro_schema(self.Config)
-
-		self.MaxBlockSize = 10  # TODO: Read this from config
-		self.Records = []
-
-
-	async def generate(self, context, event, depth):
-		self.Records.append(event)
-		if len(self.Records) < self.MaxBlockSize:
-			return None
-
-		records = self.Records
-		self.Records = []
-
-		fo = io.BytesIO()
-		fastavro.writer(fo, self.Schema, records)
-		self.Pipeline.inject(context, fo.getbuffer(), depth)
-
-
-class AvroDeserializer(bspump.Generator):
-
-	ConfigDefaults = {
-		'schema': '',
-		'schema_file': '',  # Used if 'schema is not present'
-	}
-
-
-	def __init__(self, app, pipeline, id=None, config=None):
-		super().__init__(app, pipeline, id=id, config=config)
-		self.Schema = load_avro_schema(self.Config)
-
-
-	async def generate(self, context, event, depth):
-		fi = io.BytesIO(event)
-		for record in fastavro.reader(fi, self.Schema):
-			self.Pipeline.inject(context, record, depth)
-
-
-###
 
 
 class SamplePipeline(bspump.Pipeline):
