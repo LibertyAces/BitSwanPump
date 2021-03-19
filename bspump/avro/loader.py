3bdbe80007a0edea22e11774236f1c01236fd1a6
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-03-19 09:07:54 +0100

    Add loader module

---
+++ bspump/avro/loader.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import json
+import fastavro
+import logging
+
+
+
+###
+
+L = logging.getLogger(__name__)
+
+###
+
+
+def load_avro_schema(config):
+    schema = config.get('schema')
+    if schema == '':
+        schema_file = config.get('schema_file')
+        if schema_file != '':
+            with open(schema_file, 'r') as fi:
+                schema = json.load(fi)
+
+    if schema == '':
+        raise RuntimeError("Avro scheme not read")
+
+    return fastavro.parse_schema(schema)
