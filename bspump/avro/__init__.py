7fac9e5027f996a09e6b9bb9dbff12c53f254a32
Jakub Schier <jakub.schier@gmail.com>
2021-04-06 10:30:46 +0200

    Revert "Merge branch 'master' of github.com:LibertyAces/BitSwanPump"
    This reverts commit 7977cbfb19749e18ffb96d1d0949e4de0e5b48b6, reversing
    changes made to c666bda224b6b6e526d9dd5944f955d0e50fba64.

--- bspump/avro/__init__.py
+++ bspump/avro/__init__.py
@@ -1,10 +1,7 @@
 from .sink import AvroSink
 from .source import AvroSource
-from .serializer import AvroSerializer
-from .deserializer import AvroDeserializer
+
 __all__ = (
 	'AvroSink',
 	'AvroSource',
-	'AvroSerializer',
-	'AvroDeserializer',
 )
