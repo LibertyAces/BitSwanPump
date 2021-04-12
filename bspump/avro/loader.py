5fd451823deca5195d847f80c299921a62a1931e
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-04-12 09:06:53 +0200

    Return the methond in the absence of scheme_file

--- bspump/avro/loader.py
+++ bspump/avro/loader.py
@@ -13,14 +13,9 @@ L = logging.getLogger(__name__)
 
 
 def load_avro_schema(config):
-    schema = config.get('schema')
-    if schema == '':
         schema_file = config.get('schema_file')
         if schema_file != '':
             with open(schema_file, 'r') as fi:
                 schema = json.load(fi)
-
-    if schema == '':
-        raise RuntimeError("AVRO schema is not configured.")
-
-    return fastavro.parse_schema(schema)
+            return fastavro.parse_schema(schema)
+        return
