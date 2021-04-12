37f55b2120c974026ed371b991293c56756a85ba
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-04-12 09:30:43 +0200

    method cleanup

--- bspump/avro/loader.py
+++ bspump/avro/loader.py
@@ -13,9 +13,9 @@ L = logging.getLogger(__name__)
 
 
 def load_avro_schema(config):
-        schema_file = config.get('schema_file')
-        if schema_file != '':
-            with open(schema_file, 'r') as fi:
-                schema = json.load(fi)
-            return fastavro.parse_schema(schema)
-        return
+    schema_file = config.get('schema_file')
+    if schema_file != '':
+        with open(schema_file, 'r') as fi:
+            schema = json.load(fi)
+        return fastavro.parse_schema(schema)
+
