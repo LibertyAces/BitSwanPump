4023a93622b645a4f2e0b1d632032ef2b594fda1
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-04-12 13:58:12 +0200

    return None when schema file is missing

--- bspump/avro/loader.py
+++ bspump/avro/loader.py
@@ -14,8 +14,11 @@ L = logging.getLogger(__name__)
 
 def load_avro_schema(config):
     schema_file = config.get('schema_file')
-    if schema_file != '':
+    if schema_file == '':
+        return None
+    else:
         with open(schema_file, 'r') as fi:
             schema = json.load(fi)
         return fastavro.parse_schema(schema)
+
 
