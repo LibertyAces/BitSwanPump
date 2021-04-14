68c38281bf0737e68e0d7262d0b1017cae8a6cff
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-04-14 09:14:02 +0200

    Loader : add exceptions

--- bspump/avro/loader.py
+++ bspump/avro/loader.py
@@ -23,8 +23,12 @@ def load_avro_schema(config):
     if schema_file == '':
         return None
     else:
-        with open(schema_file, 'r') as fi:
-            schema = json.load(fi)
-        return fastavro.parse_schema(schema)
+        try:
+            with open(schema_file, 'r') as fi:
+                schema = json.load(fi)
+            return fastavro.parse_schema(schema)
+        except Exception as e:
+            L.error("Schema file is incorrect")
+
 
 
