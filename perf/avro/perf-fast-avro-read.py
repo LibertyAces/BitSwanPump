368b5781e48103c73654c1bbdbea3f168b0ed67b
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-05-03 14:22:49 +0200

    fastavro : refactor Spaces to tabs

--- perf/avro/perf-fast-avro-read.py
+++ perf/avro/perf-fast-avro-read.py
@@ -16,52 +16,52 @@ L = logging.getLogger(__name__)
 class PerfSink(bspump.Sink):
 
 
-    def __init__(self, app, pipeline, id=None, config=None):
-        super().__init__(app, pipeline, id=id, config=config)
+	def __init__(self, app, pipeline, id=None, config=None):
+		super().__init__(app, pipeline, id=id, config=config)
 
-        self.Counter = 0
-        self.Mark = time.time()
+		self.Counter = 0
+		self.Mark = time.time()
 
-        app.PubSub.subscribe_all(self)
-
-
-    def process(self, context, event):
-        self.Counter += 1
+		app.PubSub.subscribe_all(self)
 
 
-    @asab.subscribe("Application.tick!")
-    def _on_tick(self, event_name):
-        now = time.time()
+	def process(self, context, event):
+		self.Counter += 1
 
-        print("EPS: {:.0f}".format(self.Counter / (now - self.Mark)))
 
-        self.Counter = 0
-        self.Mark = now
+	@asab.subscribe("Application.tick!")
+	def _on_tick(self, event_name):
+		now = time.time()
+
+		print("EPS: {:.0f}".format(self.Counter / (now - self.Mark)))
+
+		self.Counter = 0
+		self.Mark = now
 
 
 class SamplePipeline(bspump.Pipeline):
 
-    def __init__(self, app, pipeline_id):
-        super().__init__(app, pipeline_id)
+	def __init__(self, app, pipeline_id):
+		super().__init__(app, pipeline_id)
 
-        self.build(
-            bspump.avro.AvroSource(app, self, config={'path': '../data/sink.avro'}).on(bspump.trigger.PeriodicTrigger(app,1)),
-            PerfSink(app, self)
-        )
+		self.build(
+			bspump.avro.AvroSource(app, self, config={'path': '../data/sink.avro'}).on(bspump.trigger.PeriodicTrigger(app,1)),
+			PerfSink(app, self)
+		)
 
-    def on_cycle_end(self, event_name, pipeline):
-        '''
-        This ensures that at the end of the file scan, the target file is closed
-        '''
+	def on_cycle_end(self, event_name, pipeline):
+		'''
+		This ensures that at the end of the file scan, the target file is closed
+		'''
 
 
 if __name__ == '__main__':
-    app = bspump.BSPumpApplication()
+	app = bspump.BSPumpApplication()
 
-    svc = app.get_service("bspump.PumpService")
+	svc = app.get_service("bspump.PumpService")
 
-    # Construct and register Pipeline
-    pl = SamplePipeline(app, 'SamplePipeline')
-    svc.add_pipeline(pl)
+	# Construct and register Pipeline
+	pl = SamplePipeline(app, 'SamplePipeline')
+	svc.add_pipeline(pl)
 
-    app.run()
+	app.run()
