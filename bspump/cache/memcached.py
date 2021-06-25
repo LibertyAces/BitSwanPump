8c8cfc67fb2b685bd58689932fd016e62e94e4f5
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-06-25 08:09:20 +0200

    cache : memcached - constructor and for loop fix

--- bspump/cache/memcached.py
+++ bspump/cache/memcached.py
@@ -5,15 +5,15 @@ import logging
 L = logging.getLogger(__name__)
 
 
-class Memcached:
+class Memcached():
 	"""
 	Memcached for use in bspump.
 	Parameters:
 		memcache - ip:port of memcache service
 	"""
-	def __init__(self, app, max_size=1000, expiry_seconds=0, memcache=None *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		# TODO: Configurable via configfile?
+	def __init__(self, app, max_size=1000, expiry_seconds=0, memcache=None ):
+		super().__init__()
+
 		if memcache:
 			self.Client = base.Client(tuple(memcache.split(":")))
 		else:
@@ -25,7 +25,7 @@ class Memcached:
 
 
 	def set(self, to_chache: dict):
-		for key, value in to_chache:
+		for key, value in to_chache.items():
 			returned = self.Client.set(key, value, expire=self.Expiration)
 		if returned is not True:
 			L.warning("Setting a memcached key-value failed")
