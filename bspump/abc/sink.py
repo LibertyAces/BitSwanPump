38530431e8f2f3c9920383c78f50335bcf4fe608
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-08 17:20:15 +0200

    Update sink.py

--- bspump/abc/sink.py
+++ bspump/abc/sink.py
@@ -3,7 +3,6 @@ from .processor import ProcessorBase
 
 class Sink(ProcessorBase):
 	"""
-	Sink object serves as a final event destination within the pipeline given.
-	Subsequently, the event is dispatched/written into the system by the BSPump.
+
 	"""
 	pass
