6e7597461f25fb0b9214b05b2c8114bc34adbe41
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-14 18:21:25 +0200

    Update fileblocksource.py
    adding docstrings

--- bspump/file/fileblocksource.py
+++ bspump/file/fileblocksource.py
@@ -6,13 +6,25 @@ L = logging.getLogger(__file__)
 
 
 class FileBlockSource(FileABCSource):
+	"""
+	Description:
+
+	"""
 
 	def __init__(self, app, pipeline, id=None, config=None):
+		"""
+		Description:
+
+		"""
 		super().__init__(app, pipeline, id=id, config=config)
 		self.ProactorService = app.get_service("asab.ProactorService")
 
 
 	async def read(self, filename, f):
+		"""
+		Description:
+
+		"""
 		await self.Pipeline.ready()
 		# Load the file in a worker thread (to prevent blockage of the main loop)
 		worker = self.ProactorService.execute(f.read)
