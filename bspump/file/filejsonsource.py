fcbac71bf4911de64496e0e98a173586802c0fa3
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-14 18:21:33 +0200

    Update filejsonsource.py
    adding docstrings

--- bspump/file/filejsonsource.py
+++ bspump/file/filejsonsource.py
@@ -9,16 +9,25 @@ L = logging.getLogger(__file__)
 class FileJSONSource(FileABCSource):
 
 	'''
-	This file source is optimized to load even large JSONs from a file and parse that.
+	Description: This file source is optimized to load even large JSONs from a file and parse that.
 	The loading & parsing is off-loaded to the worker thread so that it doesn't block the IO loop.
+
 	'''
 
 	def __init__(self, app, pipeline, id=None, config=None):
+		'''
+		Description:
+
+		'''
 		super().__init__(app, pipeline, id=id, config=config)
 		self.ProactorService = app.get_service("asab.ProactorService")
 
 
 	async def read(self, filename, f):
+		"""
+		Description:
+		
+		"""
 		await self.Pipeline.ready()
 
 		worker = self.ProactorService.execute(json.load, f)
