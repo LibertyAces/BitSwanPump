e8a21ee6dd0cf02d072d9b482834009bdfbc65a3
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-15 18:26:07 +0200

    Update iterator.py
    changed docstrings

--- bspump/common/iterator.py
+++ bspump/common/iterator.py
@@ -8,19 +8,44 @@ L = logging.getLogger(__name__)
 
 
 class IteratorSource(TriggerSource):
+	"""
+	Description:
+
+
+	"""
 
 	def __init__(self, app, pipeline, iterator: collections.abc.Iterator, id=None, config=None):
+		"""
+		Description:
+
+
+		"""
 		super().__init__(app, pipeline, id=id, config=config)
 		self.Iterator = iterator
 
 
 	async def cycle(self, *args, **kwags):
+		"""
+		Description:
+
+
+		"""
 		for event in self.Iterator:
 			await self.process(event)
 
 
 class IteratorGenerator(Generator):
+	"""
+	Description:
+
+
+	"""
 
 	async def generate(self, context, event, depth):
+		"""
+		Description:
+
+
+		"""
 		for item in event:
 			self.Pipeline.inject(context, item, depth)
