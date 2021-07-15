e051543c95adc6e0df9aac94f5f1512c90fd4523
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-15 18:26:11 +0200

    Update mapping.py
    changed docstrings

--- bspump/common/mapping.py
+++ bspump/common/mapping.py
@@ -6,41 +6,97 @@ from ..abc.generator import Generator
 
 class MappingKeysProcessor(Processor):
 	"""
-	Mapping Keys Processor
+	Description: Mapping Keys Processor
+
 	"""
 	def process(self, context, event: collections.abc.Mapping) -> list:
 		"""
-		process is a method of a Mapping Keys Processor
+		Description: process is a method of a Mapping Keys Processor
+
+		:return: *event.keys()
+
+		|
+
 		"""
 		return [*event.keys()]
 
 
 class MappingValuesProcessor(Processor):
+	"""
+	Description:
+
+	"""
+
 	def process(self, context, event: collections.abc.Mapping) -> list:
+		"""
+		Description:
+
+		:return: *event.values()
+
+		|
+
+		"""
 		return [*event.values()]
 
 
 class MappingItemsProcessor(Processor):
+	"""
+	Description:
+
+	"""
+
 	def process(self, context, event: collections.abc.Mapping) -> list:
+		"""
+		Description:
+
+		:return: *event.items()
+
+		|
+
+		"""
 		return [*event.items()]
 
 
 class MappingKeysGenerator(Generator):
+	"""
+	Description:
+
+	"""
 
 	async def generate(self, context, event, depth):
+		"""
+		Description:
+
+		"""
 		for item in event.keys():
 			self.Pipeline.inject(context, item, depth)
 
 
 class MappingValuesGenerator(Generator):
+	"""
+	Description:
+
+	"""
 
 	async def generate(self, context, event, depth):
+		"""
+		Description:
+
+		"""
 		for item in event.values():
 			self.Pipeline.inject(context, item, depth)
 
 
 class MappingItemsGenerator(Generator):
+	"""
+	Description:
+
+	"""
 
 	async def generate(self, context, event, depth):
+		"""
+		Description:
+
+		"""
 		for item in event.items():
 			self.Pipeline.inject(context, item, depth)
