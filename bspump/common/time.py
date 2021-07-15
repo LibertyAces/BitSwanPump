141e97bafad942d97190dbb347b590804b44db71
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-15 18:26:24 +0200

    Update time.py
    changed docstrings

--- bspump/common/time.py
+++ bspump/common/time.py
@@ -10,7 +10,8 @@ L = logging.getLogger(__name__)
 
 class TimeZoneNormalizer(bspump.Processor):
 	"""
-	Normalizes datetime from local timezone (e.g. in config) to UTC, which is preferred internal datetime form
+	Description: Normalizes datetime from local timezone (e.g. in config) to UTC, which is preferred internal datetime form
+
 	"""
 
 	ConfigDefaults = {
@@ -19,16 +20,22 @@ class TimeZoneNormalizer(bspump.Processor):
 
 
 	def __init__(self, app, pipeline, id=None, config=None):
+		"""
+		Description:
+
+		"""
 		super().__init__(app, pipeline, id, config)
 		self.TimeZoneSource = pytz.timezone(self.Config.get("timezone"))
 
 
 	def normalize(self, local_time: datetime.datetime) -> datetime.datetime:
 		"""
-		If `local_time` doesn't contain a time zone (e.g. it is naive), the timezone will be added from config
+		Description: 	If `local_time` doesn't contain a time zone (e.g. it is naive), the timezone will be added from config
 
-		:param local_time: Local time to normalize
 		:return: Normalized `local_time` in UTC
+
+		|
+
 		"""
 		if not local_time.tzinfo:
 			local_time = self.TimeZoneSource.localize(local_time)
@@ -37,7 +44,7 @@ class TimeZoneNormalizer(bspump.Processor):
 
 	def process(self, context, event):
 		"""
-		Abstract method to process the event. Must be customized.
+		Description: Abstract method to process the event. Must be customized.
 
 		Example:
 
