430491a291b277356a59df2f1f7eaf5cdc272c76
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-15 18:26:09 +0200

    Update json.py
    changed docstrings

--- bspump/common/json.py
+++ bspump/common/json.py
@@ -5,43 +5,95 @@ from ..abc.processor import Processor
 
 class CySimdJsonParser(Processor):
 	'''
-	Fast JSON parser. Expects json bytes represented as bytes as input
+	Description: Fast JSON parser. Expects json bytes represented as bytes as input
 	Based on https://github.com/TeskaLabs/cysimdjson
+
 	'''
 
 	def __init__(self, app, pipeline, id=None, config=None):
+		"""
+		Description: .
+
+
+		"""
 		super().__init__(app, pipeline, id, config)
 		self._parser = cysimdjson.JSONParser()
 
 	def process(self, context, event: bytes):
+		"""
+		Description:
+
+		:return: self._parser.parse(event)
+
+		"""
 		return self._parser.parse(event)
 
 
 class StdDictToJsonParser(Processor):
+	"""
+	Description:
+
+	"""
 
 	def process(self, context, event):
+		"""
+		Description:
+
+		:return: ?
+
+		|
+
+		"""
 		return json.dumps(event)
 
 
 class StdJsonToDictParser(Processor):
 
+	"""
+	Description:
+
+
+	"""
+
 	def process(self, context, event):
+		"""
+		Description:
+
+		:return: ???
+
+		|
+
+		"""
 		return json.loads(event)
 
 
 class DictToJsonBytesParser(Processor):
 	"""
-	DictToJsonBytesParser transforms a dictionary to JSON-string encoded in bytes.
+	Description: DictToJsonBytesParser transforms a dictionary to JSON-string encoded in bytes.
 	The encoding charset can be specified in the configuration in `encoding` field.
+
 	"""
 	ConfigDefaults = {
 		'encoding': 'utf-8',
 	}
 
 	def __init__(self, app, pipeline, id=None, config=None):
+		"""
+		Description: ..
+
+
+		"""
 		super().__init__(app, pipeline, id, config)
 		self.Encoding = self.Config['encoding']
 
 	def process(self, context, event):
+		"""
+		Description:
+
+		:return: ??
+
+		|
+
+		"""
 		assert isinstance(event, dict)
 		return json.dumps(event).encode(self.Encoding)
