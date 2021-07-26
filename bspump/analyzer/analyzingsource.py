bb7dcbec622e6ac04a3495378e4bfcd4f8579924
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-26 13:49:28 +0200

    Update analyzingsource.py

--- bspump/analyzer/analyzingsource.py
+++ bspump/analyzer/analyzingsource.py
@@ -8,13 +8,17 @@ L = logging.getLogger(__name__)
 
 class AnalyzingSource(TriggerSource):
 	'''
-		The `AnalyzingSource` is triggered source, which expects `matrix_id` as an input.
-		Each trigger fire it calls `analyze()` function of the `Matrix` and expects
-		a complex event as an output. A complex event can be array of events, aggregation of events (average, max, min etc.)
+	The `AnalyzingSource` is triggered source, which expects `matrix_id` as an input.
+	Each trigger fire it calls `analyze()` function of the `Matrix` and expects
+	a complex event as an output. A complex event can be array of events, aggregation of events (average, max, min etc.)
 	'''
 
 
 	def __init__(self, app, pipeline, matrix_id, id=None, config=None):
+		"""
+		Description:
+
+		"""
 		super().__init__(app, pipeline, id=id, config=config)
 		svc = app.get_service("bspump.PumpService")
 		self.AnalyzeMatrix = svc.locate_matrix(matrix_id)
@@ -22,5 +26,9 @@ class AnalyzingSource(TriggerSource):
 
 
 	async def cycle(self):
+		"""
+		Description:
+
+		"""
 		event = await self.AnalyzeMatrix.analyze()
 		await self.process(event)
