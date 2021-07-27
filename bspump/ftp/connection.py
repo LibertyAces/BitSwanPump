b555cdc89a9b283baa64332d2e8284842cf28f52
FrodoCZE <30985565+FrodoCZE@users.noreply.github.com>
2021-07-27 11:47:16 +0200

    Update connection.py
    added new ftp section

--- bspump/ftp/connection.py
+++ bspump/ftp/connection.py
@@ -12,6 +12,12 @@ L = logging.getLogger(__name__)
 
 
 class FTPConnection(Connection):
+	"""
+	Description:
+
+	|
+
+	"""
 
 	ConfigDefaults = {
 		'hostname': 'localhost',
@@ -19,6 +25,12 @@ class FTPConnection(Connection):
 	}
 
 	def __init__(self, app, id=None, config=None):
+		"""
+		Description:
+
+		|
+
+		"""
 		super().__init__(app, id=id, config=config)
 
 		self.Username = self.Config.get('username')
@@ -27,6 +39,14 @@ class FTPConnection(Connection):
 		self.Port = self.Config.get('port')
 
 	async def connect(self):
+		"""
+		Description:
+
+		:return: client
+
+		|
+
+		"""
 		client = aioftp.Client()
 		await client.connect(self.Hostname,21)
 		await client.login(self.Username, self.Password)
