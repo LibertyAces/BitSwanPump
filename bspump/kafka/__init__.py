52b7bc5bb6b30a9d09efe9e5e70371bea5de0c1b
Premysl Cerny <premysl.cerny@teskalabs.com>
2022-05-26 13:26:23 +0200

    Remove import of Kafka Initializer

--- bspump/kafka/__init__.py
+++ bspump/kafka/__init__.py
@@ -2,12 +2,10 @@ from .connection import KafkaConnection
 from .source import KafkaSource
 from .sink import KafkaSink
 from .keyfilter import KafkaKeyFilter
-from .topic_initializer import KafkaTopicInitializer
 
 __all__ = [
 	"KafkaConnection",
 	"KafkaSource",
 	"KafkaSink",
 	"KafkaKeyFilter",
-	"KafkaTopicInitializer",
 ]
