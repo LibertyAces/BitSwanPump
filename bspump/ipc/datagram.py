6a12fde7363465900215a044e7d0db56a04d27d8
Ales Teska <ales.teska@teskalabs.com>
2023-05-30 14:59:07 +0200

    Fixes and improvements in IPC / Socket

--- bspump/ipc/datagram.py
+++ bspump/ipc/datagram.py
@@ -1,9 +1,11 @@
+import sys
 import asyncio
 import logging
 import socket
 
 from ..abc.source import Source
 from ..abc.sink import Sink
+from .utils import parse_address
 
 #
 
@@ -14,8 +16,8 @@ L = logging.getLogger(__name__)
 
 class DatagramSource(Source):
 	"""
-	Description:
-
+	Description: a datagram source that creates a UDP socket and receives datagrams.
+	It is usable also with UNIX datagram sockets.
 	"""
 
 
@@ -27,25 +29,26 @@ class DatagramSource(Source):
 
 
 	def __init__(self, app, pipeline, id=None, config=None):
-		"""
-		Description:
-
-
-		"""
 		super().__init__(app, pipeline, id=id, config=config)
-		self.Loop = app.Loop
 
-		# Create a UDP socket
-		self.Address = str(self.Config['address'])
-
-		# Receive Buffer Size
 		self.ReceiveBufferSize = int(self.Config['receiver_buffer_size'])
+		self.MaxPacketSize = int(self.Config['max_packet_size'])
 
-		addrline = self.Address.strip()
-		if addrline.count(":") == 1:
-			host, port = self.Address.rsplit(":", maxsplit=1)
-			(family, socktype, proto, canonname, sockaddr) = socket.getaddrinfo(host, port)[0]
+		self.Address = str(self.Config['address'])
+		addr_family, addr = parse_address(self.Address)
 
+		if addr_family == socket.AF_UNIX:
+			self.Socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+			self.Socket.setblocking(False)
+			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+			if self.ReceiveBufferSize > 0:
+				self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.ReceiveBufferSize)
+
+			self.Socket.bind(addr)
+
+		else:  # Internet address family
+			(family, socktype, proto, canonname, sockaddr) = socket.getaddrinfo(addr[0], addr[1])[0]
 			self.Socket = socket.socket(family, socket.SOCK_DGRAM)
 			self.Socket.setblocking(False)
 			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -55,36 +58,17 @@ class DatagramSource(Source):
 
 			self.Socket.bind(sockaddr)
 
-		else:
-
-			self.Socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-			self.Socket.setblocking(False)
-			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-			if self.ReceiveBufferSize > 0:
-				self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.ReceiveBufferSize)
-
-			self.Socket.bind(self.Address)
-
-		self.MaxPacketSize = int(self.Config['max_packet_size'])
-
 
 	async def main(self):
-		task = asyncio.ensure_future(self._receive())
+		if sys.version_info < (3, 11):
+			return await self._main_legacy()
 
-		await self.stopped()
+		loop = self.Pipeline.App.Loop
 
-		task.cancel()
-		await task
-
-		self.Socket.close()
-
-
-	async def _receive(self):
 		while True:
 			try:
 				await self.Pipeline.ready()
-				event, peer = await self.Loop.sock_recvfrom(self.Socket, self.MaxPacketSize)
+				event, peer = await loop.sock_recvfrom(self.Socket, self.MaxPacketSize)
 				await self.Pipeline.ready()
 				await self.process(event, context={'datagram': peer})
 
@@ -96,70 +80,59 @@ class DatagramSource(Source):
 				raise
 
 
+	async def _main_legacy(self):
+		loop = self.Pipeline.App.Loop
+		while True:
+			try:
+				await self.Pipeline.ready()
+				event = await loop.sock_recv(self.Socket, self.MaxPacketSize)
+				await self.process(event, context={'datagram': None})
+
+			except asyncio.CancelledError:
+				break
+
+			except Exception:
+				L.exception("Error in datagram source.")
+				raise
+
+
 class DatagramSink(Sink):
 	"""
-	Description:
-
-
+	Description: Outbound UDP and UNIX datagram socket.
 	"""
 
 
 	ConfigDefaults = {
 		'address': '127.0.0.1 8888',  # IPv4, IPv6 or unix socket path
-		'max_packet_size': 64 * 1024,
-		'receiver_buffer_size': 0,
 	}
 
 
 	def __init__(self, app, pipeline, id=None, config=None):
-		"""
-		Description:
-
-
-		"""
-
 		super().__init__(app, pipeline, id=id, config=config)
 		self.Loop = app.Loop
 
-		# Create a UDP socket
 		self.Address = str(self.Config['address'])
+		addr_family, addr = parse_address(self.Address)
 
-		# Receive Buffer Size
-		self.ReceiveBufferSize = int(self.Config['receiver_buffer_size'])
 
-		addrline = self.Address.strip()
-		if " " in addrline:
-			host, port = self.Address.rsplit(" ", maxsplit=1)
-		elif addrline.count(":") == 1:
-			host, port = self.Address.rsplit(":", maxsplit=1)
-			(family, socktype, proto, canonname, sockaddr) = socket.getaddrinfo(host, port)[0]
+		if addr_family == socket.AF_UNIX:
+			self.Socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+			self.Socket.setblocking(False)
+			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+			self.Socket.connect(addr)
+
+		else:  # Internet address family
+			(family, socktype, proto, canonname, sockaddr) = socket.getaddrinfo(addr[0], addr[1])[0]
 
 			self.Socket = socket.socket(family, socket.SOCK_DGRAM)
 			self.Socket.setblocking(False)
 			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-			if self.ReceiveBufferSize > 0:
-
-				self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.ReceiveBufferSize)
 
 			self.Socket.connect(sockaddr)
-		else:
-			self.Socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-			self.Socket.setblocking(False)
-			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-			if self.ReceiveBufferSize > 0:
-				self.Socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.ReceiveBufferSize)
-
-			self.Socket.connect(self.Address)
-
-		self.MaxPacketSize = int(self.Config['max_packet_size'])
 
 
 	def process(self, context, event):
-		"""
-		Description:
-
-
-		"""
 		self.Socket.send(event)
