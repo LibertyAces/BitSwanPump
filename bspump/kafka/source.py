4f968e7fc49eff123e06f0dc86761ddd7cf25214
Premysl Cerny <premysl.cerny@teskalabs.com>
2024-08-12 10:45:00 +0200

    Update source.py

--- bspump/kafka/source.py
+++ bspump/kafka/source.py
@@ -248,9 +248,9 @@ class KafkaSource(Source):
 
 					except confluent_kafka.KafkaException as err:
 
-						# Reballacing of partitions happens during consuming,
+						# Reballacing of partitions happened during consuming,
 						# so this consumer no longer owns the partition
-						# -> let the other consumer consume the events and just finish
+						# -> let the other consumer consume the events and here just finish
 						# https://medium.com/@a.a.halutin/simple-examples-with-confluent-kafka-9b7e58534a88
 						if err.args[0].code() == confluent_kafka.KafkaError._STATE and err.args[0].val() == -172:
 
