f226f12ecd5da4d1886a96925d8714bcec9d9123
Premysl Cerny <premysl.cerny@teskalabs.com>
2024-08-12 10:43:38 +0200

    Update source.py

--- bspump/kafka/source.py
+++ bspump/kafka/source.py
@@ -248,8 +248,12 @@ class KafkaSource(Source):
 
 					except confluent_kafka.KafkaException as err:
 
+						# Reballacing of partitions happens during consuming,
+						# so this consumer no longer owns the partition
+						# -> let the other consumer consume the events and just finish
 						# https://medium.com/@a.a.halutin/simple-examples-with-confluent-kafka-9b7e58534a88
 						if err.args[0].code() == confluent_kafka.KafkaError._STATE and err.args[0].val() == -172:
+							consumer.commit(asynchronous=False)  # Commit offsets synchronously
 							break
 
 						L.warning("The following warning occurred inside Kafka consumer: '{}'".format(err))
