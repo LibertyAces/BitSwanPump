16eb2e044ab461e5e7f07167041c43ba82761b52
Ales Teska <ales.teska@teskalabs.com>
2024-11-28 22:59:57 +0100

    Disabling automated import of Analyzers.

--- bspump/analyzer/__init__.py
+++ bspump/analyzer/__init__.py
@@ -1,20 +1,20 @@
-from .analyzer import Analyzer
-from .timewindowanalyzer import TimeWindowAnalyzer
-from .timedriftanalyzer import TimeDriftAnalyzer
-from .sessionanalyzer import SessionAnalyzer
-from .geoanalyzer import GeoAnalyzer
-from .latch import LatchAnalyzer
-from .analyzingsource import AnalyzingSource
-from .threshold import ThresholdAnalyzer
+# from .analyzer import Analyzer
+# from .timewindowanalyzer import TimeWindowAnalyzer
+# from .timedriftanalyzer import TimeDriftAnalyzer
+# from .sessionanalyzer import SessionAnalyzer
+# from .geoanalyzer import GeoAnalyzer
+# from .latch import LatchAnalyzer
+# from .analyzingsource import AnalyzingSource
+# from .threshold import ThresholdAnalyzer
 
 
-__all__ = (
-	'Analyzer',
-	'TimeWindowAnalyzer',
-	'TimeDriftAnalyzer',
-	'SessionAnalyzer',
-	'GeoAnalyzer',
-	'LatchAnalyzer',
-	'AnalyzingSource',
-	'ThresholdAnalyzer',
-)
+# __all__ = (
+# 	'Analyzer',
+# 	'TimeWindowAnalyzer',
+# 	'TimeDriftAnalyzer',
+# 	'SessionAnalyzer',
+# 	'GeoAnalyzer',
+# 	'LatchAnalyzer',
+# 	'AnalyzingSource',
+# 	'ThresholdAnalyzer',
+# )
