482cd18bac72946cd667feb93912eb35b0021b6c
mburysek <miroslav.burysek@teskalabs.com>
2025-01-07 09:59:23 +0100

    Improve CSV example

--- examples/bspump-csv.py
+++ examples/bspump-csv.py
@@ -13,38 +13,48 @@ L = logging.getLogger(__name__)
 ###
 
 
-class SamplePipeline(bspump.Pipeline):
-
+class SampleCSVPipeline(bspump.Pipeline):
 	def __init__(self, app, pipeline_id):
 		super().__init__(app, pipeline_id)
 
-		self.Sink = bspump.file.FileCSVSink(app, self, config={'path': './data/out.csv'})
-
-		self.build(
-			bspump.file.FileCSVSource(
-				app, self, config={'path': './data/sample.csv', 'delimiter': ';', 'post': 'noop'}
-			).on(bspump.trigger.RunOnceTrigger(app)),
-			bspump.common.PPrintProcessor(app, self),
-			self.Sink
+		self.Source = bspump.file.FileCSVSource(
+			app,
+			self,
+			config={
+				"path": "./examples/data/countries.csv",
+				"delimiter": ";",
+				"post": "noop",  # one of 'delete', 'noop' and 'move'
+			},
+		)
+		self.Sink = bspump.file.FileCSVSink(
+			app, self, config={"path": "./examples/data/countries-out.csv"}
 		)
 
+		self.build(
+			self.Source.on(bspump.trigger.RunOnceTrigger(app)),
+			bspump.common.PPrintProcessor(app, self),
+			self.Sink,
+		)
+
+		self.PubSub.subscribe("bspump.file_source.no_files!", self.on_no_files)
 		self.PubSub.subscribe("bspump.pipeline.cycle_end!", self.on_cycle_end)
 
-
 	def on_cycle_end(self, event_name, pipeline):
-		'''
-		This ensures that at the end of the file scan, the target file is closed
-		'''
+		"""
+		This ensures that at the end of the file scan, the target file is closed.
+		"""
 		self.Sink.rotate()
 
+	def on_no_files(self, event_name, pipeline):
+		L.warning("No files found in the selected path.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 	app = bspump.BSPumpApplication()
-
-	svc = app.get_service("bspump.PumpService")
+	svc: bspump.BSPumpService = app.get_service("bspump.PumpService")
 
 	# Construct and register Pipeline
-	pl = SamplePipeline(app, 'SamplePipeline')
-	svc.add_pipeline(pl)
+	pipeline = SampleCSVPipeline(app, "SampleCSVPipeline")
+	svc.add_pipeline(pipeline)
 
 	app.run()
