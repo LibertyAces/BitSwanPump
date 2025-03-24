1d69b97ff3a158f8b8de40943fbe6b86abd9cfca
Premysl Cerny <premysl.cerny@teskalabs.com>
2025-03-24 11:33:30 +0100

    Add HTTPClientCSVSource

--- bspump/http/__init__.py
+++ bspump/http/__init__.py
@@ -1,16 +1,19 @@
 from .client.source import HTTPClientLineSource
 from .client.source import HTTPClientSource
 from .client.source import HTTPClientTextSource
+from .client.source import HTTPClientCSVSource
 from .client.wssink import HTTPClientWebSocketSink
 from .web.sink import WebServiceSink
 from .web.source import WebServiceSource
 from .web.wssource import WebSocketSource
 from .lookupprovider import HTTPBatchLookupProvider
 
+
 __all__ = (
 	'HTTPClientSource',
 	'HTTPClientTextSource',
 	'HTTPClientLineSource',
+	'HTTPClientCSVSource',
 	'HTTPClientWebSocketSink',
 	'WebServiceSource',
 	'WebServiceSink',
--- bspump/http/client/source.py
+++ bspump/http/client/source.py
@@ -1,3 +1,6 @@
+import csv
+import os
+
 from .abcsource import HTTPABCClientSource
 
 
@@ -33,4 +36,88 @@ class HTTPClientLineSource(HTTPClientTextSource):
 		response = await response.text(encoding=self.encoding)
 
 		for line in response.split('\n'):
+			await self.process(line)
+
+
+class HTTPClientCSVSource(HTTPClientTextSource):
+
+	ConfigDefaults = {
+		'encoding': '',
+		'mode': 'r',
+		'newline': '',  # Required by CSV parser
+		'dialect': 'excel',
+		'delimiter': ',',
+		'doublequote': True,
+		'escapechar': "\\",  # Python >3.11 does not allow empty value
+		'lineterminator': os.linesep,
+		'quotechar': '"',
+		'quoting': csv.QUOTE_MINIMAL,
+		'skipinitialspace': False,
+		'strict': False,
+	}
+
+
+	def __init__(self, app, pipeline, id=None, config=None, headers={}):
+		super().__init__(app, pipeline, id=id, config=config, headers={})
+		self.Dialect = csv.get_dialect(self.Config['dialect'])
+
+
+	def reader(self, f):
+		"""
+		Description:
+
+		**Parameters**
+
+		f :
+
+		:returns: ??
+
+		|
+
+		"""
+		kwargs = {}
+
+		v = self.Config.get('delimiter')
+		if v is not None:
+			kwargs['delimiter'] = v
+
+		v = self.Config.get('doublequote')
+		if v is not None:
+			kwargs['doublequote'] = v
+
+		v = self.Config.get('escapechar')
+		if v is not None:
+			kwargs['escapechar'] = v
+
+		v = self.Config.get('lineterminator')
+		if v is not None:
+			kwargs['lineterminator'] = v
+
+		v = self.Config.get('quotechar')
+		if v is not None:
+			kwargs['quotechar'] = v
+
+		v = self.Config.get('quoting')
+		if v is not None:
+			kwargs['quoting'] = v
+
+		v = self.Config.get('skipinitialspace')
+		if v is not None:
+			kwargs['skipinitialspace'] = v
+
+		v = self.Config.get('strict')
+		if v is not None:
+			kwargs['strict'] = v
+
+		return csv.DictReader(
+			f,
+			dialect=self.Dialect,
+			**kwargs
+		)
+
+
+	async def read(self, response):
+		response = await response.text(encoding=self.encoding)
+
+		for line in self.reader(response):
 			await self.process(line)
